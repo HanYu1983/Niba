73e1b1541fd7b9e4ad5f1ee65b3853ee09a19af0
HanYu <hanyushome@gmail.com>
2017-10-07 20:50:58 +0800

    no message

---
+++ android/NibaUnity/src/main/java/com/hanvic/niba/UnityPlayerActivity.java
@@ -0,0 +1,138 @@
+package com.hanvic.niba;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.Window;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.Scope;
+import com.unity3d.player.UnityPlayer;
+
+public class UnityPlayerActivity extends FragmentActivity implements OnConnectionFailedListener
+{
+	protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+
+	GoogleApiClient mGoogleApiClient;
+	// Setup activity layout
+	@Override protected void onCreate (Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+
+		getWindow().setFormat(PixelFormat.RGBX_8888); // <--- This makes xperia play happy
+
+		mUnityPlayer = new UnityPlayer(this);
+		setContentView(mUnityPlayer);
+		mUnityPlayer.requestFocus();
+
+		OnConnectionFailedListener lis;
+
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+				//.requestServerAuthCode("241640252242-e9962fc449bb9sh847eoqlt5oshef34g.apps.googleusercontent.com")
+				.requestEmail()
+				.requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+				.build();
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.enableAutoManage(this, this)
+				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+				.build();
+
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+		startActivityForResult(signInIntent, 1011);
+	}
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		Log.d("onConnectionFailed", result.getErrorMessage());
+		AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
+		MyAlertDialog.setTitle("onConnectionFailed");
+		MyAlertDialog.setMessage(result.getErrorMessage());
+		MyAlertDialog.show();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == 1011) {
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+			if (result.isSuccess()) {
+				GoogleSignInAccount acct = result.getSignInAccount();
+				String token = acct.getIdToken();
+
+				AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
+				MyAlertDialog.setTitle("token");
+				MyAlertDialog.setMessage(token);
+				MyAlertDialog.show();
+			} else {
+				AlertDialog.Builder MyAlertDialog = new AlertDialog.Builder(this);
+				MyAlertDialog.setTitle("error");
+				MyAlertDialog.setMessage("fail:"+result.getStatus().getStatusCode());
+				MyAlertDialog.show();
+			}
+		}
+	}
+
+
+	// Quit Unity
+	@Override protected void onDestroy ()
+	{
+		mUnityPlayer.quit();
+		super.onDestroy();
+	}
+
+	// Pause Unity
+	@Override protected void onPause()
+	{
+		super.onPause();
+		mUnityPlayer.pause();
+	}
+
+	// Resume Unity
+	@Override protected void onResume()
+	{
+		super.onResume();
+		mUnityPlayer.resume();
+	}
+
+	// This ensures the layout will be correct.
+	@Override public void onConfigurationChanged(Configuration newConfig)
+	{
+		super.onConfigurationChanged(newConfig);
+		mUnityPlayer.configurationChanged(newConfig);
+	}
+
+	// Notify Unity of the focus change.
+	@Override public void onWindowFocusChanged(boolean hasFocus)
+	{
+		super.onWindowFocusChanged(hasFocus);
+		mUnityPlayer.windowFocusChanged(hasFocus);
+	}
+
+	// For some reason the multiple keyevent type is not supported by the ndk.
+	// Force event injection by overriding dispatchKeyEvent().
+	@Override public boolean dispatchKeyEvent(KeyEvent event)
+	{
+		if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+			return mUnityPlayer.injectEvent(event);
+		return super.dispatchKeyEvent(event);
+	}
+
+	// Pass any events not handled by (unfocused) views straight to UnityPlayer
+	@Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+	@Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+	@Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+	/*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
+}
