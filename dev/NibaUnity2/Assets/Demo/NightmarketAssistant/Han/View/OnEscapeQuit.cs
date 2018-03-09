65d869f4c9d8f5da79b1d9fad23ce2a3bdfef16c
HanYu1983 <hanyushome@gmail.com>
2018-03-09 12:58:00 +0800

    no message

---
+++ dev/NibaUnity2/Assets/Demo/NightmarketAssistant/Han/View/OnEscapeQuit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NightmarketAssistant
+{
+    public class OnEscapeQuit : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyUp("escape"))
+            {
+#if UNITY_EDITOR
+                Debug.LogWarning("退出應用");
+#endif
+                Application.Quit();
+            }
+        }
+    }
+}
