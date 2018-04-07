4574016bf85214b281ff560015aec8f0a09cba9e
HanYu1983 <hanyushome@gmail.com>
2018-04-07 16:04:58 +0800

    no message

---
+++ dev/NibaUnity2/Assets/HanUtil/PageManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HanUtil
+{
+    public class PageManager : MonoBehaviour
+    {
+        public List<string> names;
+        public string[] paths;
+        public Transform root;
+        public string startPage;
+
+        public GameObject currPage;
+
+        private void Start()
+        {
+            if (string.IsNullOrEmpty(startPage) == false)
+            {
+                ChangePage(startPage);
+            }
+        }
+
+        public IEnumerator UnloadPageAsync()
+        {
+            if(currPage != null)
+            {
+                Destroy(currPage);
+                currPage = null;
+            }
+            yield return 0;
+        }
+
+        public IEnumerator ChangePageAsync(string name)
+        {
+            var idx = names.IndexOf(name);
+            if(idx == -1)
+            {
+                throw new System.Exception("no page:"+name);
+            }
+            yield return UnloadPageAsync();
+            var req = Resources.LoadAsync(paths[idx]);
+            yield return req;
+            var prefab = req.asset;
+            var page = Instantiate(prefab, root, false) as GameObject;
+            page.SetActive(true);
+            currPage = page;
+        }
+        
+        public void ChangePage(string name)
+        {
+            StartCoroutine(ChangePageAsync(name));
+        }
+    }
+}
---
+++ dev/NibaUnity2/Assets/HanUtil/SendAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace HanUtil
+{
+    public class SendAction : MonoBehaviour
+    {
+        public static Action<string> OnEvent = delegate { };
+        public void Notify(string evt)
+        {
+            Debug.Log("SendAction-Notify:"+evt);
+            OnEvent(evt);
+        }
+    }
+}
