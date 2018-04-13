e85da3d176a26f19a282d936cde34d702966ad07
HanYu1983 <hanyushome@gmail.com>
2018-04-13 21:37:44 +0800

    no message

---
+++ dev/NibaUnity2/Assets/Demo/RobotWar/Controller/Network/MultiController.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework.GameStructure;
+using GameFramework.UI.Dialogs.Components;
+using System.Linq;
+
+namespace RobotWar
+{
+    public class MultiController : DefaultController
+    {
+        public View view;
+        public Model model;
+        public RobotWarClient client;
+
+        void Start()
+        {
+            // GameFramework的Pause按鈕一按下去後Time.timeScale會設為0, 之後就變不回為1
+            // 還沒去研究GameFramework的正確使用方式, 先在這裡用偷懒解法
+            Time.timeScale = 1;
+            model = GameManager.Instance.gameObject.GetComponent<Model>();
+        }
+
+        void Update()
+        {
+            Step(Time.deltaTime);
+        }
+
+        public void StartPlay()
+        {
+            if(Player == 0)
+            {
+                ChangeState(new SystemState());
+            }
+        }
+
+        public override void ServerAlart(string title, string msg)
+        {
+            client.ServerAlert(title, msg);
+        }
+
+        public override void ClientMoveUnit(int playerId, string unit, Vector2Int pos)
+        {
+            client.CmdMoveUnit(playerId, unit, new Grid(pos).Key);
+        }
+        public override void ClientCancelMoveUnit(int playerId, string unit)
+        {
+            client.CmdCancelMoveUnit(playerId, unit);
+        }
+        public override void ClientPushTask(int playerId, Task task, bool isAttack)
+        {
+            client.CmdPushTask(playerId, JsonUtility.ToJson(task), isAttack);
+        }
+        public override void ClientSetUnitDirection(int playerId, string unit, Direction dir)
+        {
+            client.CmdSetUnitDirection(playerId, unit, dir);
+        }
+        public override void ClientPassUnit(int playerId, string unit)
+        {
+            client.CmdPassUnit(playerId, unit);
+        }
+
+        #region IController impl
+        public override Model Model { get { return model; } }
+        public override View View { get { return view; } }
+        public override int Player
+        {
+            get { return client.playerId; }
+        }
+        #endregion
+
+        #region test
+        IEnumerator CreateMap(string path)
+        {
+            /*
+            var request = Resources.LoadAsync<MapData>(path);
+            yield return request;
+            var data = request.asset as MapData;
+            model.CreateMap(data);
+            view.Sync(model);
+            */
+            DataAlg.GenMap(model.mapCtx, 20, 20);
+            view.Sync(model);
+            yield return null;
+        }
+
+        void CreateUnit(int owner, Vector2Int pos)
+        {
+            var unit = DataAlg.CreateUnit(model.mapCtx, ConfigUnit.ID_test01);
+            var w = DataAlg.CreateWeapon(model.mapCtx, ConfigWeapon.ID_handGun);
+            DataAlg.AssignWeapon(model.mapCtx, w.Key, unit.Key);
+            DataAlg.PutUnit(model.mapCtx, pos, owner, unit.Key);
+
+            w = DataAlg.CreateWeapon(model.mapCtx, ConfigWeapon.ID_lightSword);
+            DataAlg.AssignWeapon(model.mapCtx, w.Key, unit.Key);
+
+            w = DataAlg.CreateWeapon(model.mapCtx, ConfigWeapon.ID_bomb);
+            DataAlg.AssignWeapon(model.mapCtx, w.Key, unit.Key);
+
+            w = DataAlg.CreateWeapon(model.mapCtx, ConfigWeapon.ID_bigGun);
+            DataAlg.AssignWeapon(model.mapCtx, w.Key, unit.Key);
+
+            var p = DataAlg.CreatePilot(model.mapCtx, ConfigPilot.ID_solider1);
+            DataAlg.AssignPilot(model.mapCtx, p.Key, unit.Key);
+
+            view.CreateUnit(model, unit.Key, pos);
+        }
+
+        [ContextMenu("TestLoadMap")]
+        public void TestLoadMap()
+        {
+            StartCoroutine(CreateMap("Map/map01"));
+        }
+        [ContextMenu("TestPlay")]
+        public void TestPlay()
+        {
+            StartPlay();
+        }
+        [ContextMenu("TestCreateUnit")]
+        public void TestCreateUnit()
+        {
+            var p = DataAlg.CreatePlayer(model.mapCtx, 0, false);
+            for (var i = 0; i < 5; ++i)
+            {
+                CreateUnit(p.Key, new Vector2Int(Random.Range(0, 10), Random.Range(0, 10)));
+            }
+        }
+
+        [ContextMenu("TestCreateEnemy")]
+        public void TestCreateEnemy()
+        {
+            var p = DataAlg.CreatePlayer(model.mapCtx, 1, false);
+            for (var i = 0; i < 5; ++i)
+            {
+                CreateUnit(p.Key, new Vector2Int(Random.Range(0, 10), Random.Range(0, 10)));
+            }
+        }
+
+        [ContextMenu("TestCanMove")]
+        public void TestCanMove()
+        {
+            view.SetGridColor(null, Color.white);
+
+            var paths = DataAlg.FindAllPath(model.mapCtx, Random.Range(3, 10), new Vector2Int(Random.Range(0, 10), Random.Range(0, 10)));
+            view.SetGridColor(paths.Keys, Color.green);
+
+            var dis = new Grid[paths.Keys.Count];
+            paths.Keys.CopyTo(dis, 0);
+            var path = paths[dis[Random.Range(1, dis.Length)]];
+            view.SetGridColor(path, Color.red);
+        }
+
+        [ContextMenu("TestAttackRange")]
+        public void TestAttackRange()
+        {
+            view.SetGridColor(null, Color.white);
+            var paths = DataAlg.FindAllRange(model.mapCtx, Random.Range(1, 5), Random.Range(5, 10), new Vector2Int(Random.Range(0, 10), Random.Range(0, 10)));
+            view.SetGridColor(paths.Keys, Color.red);
+        }
+
+        [ContextMenu("TestPolar")]
+        public void TestPolar()
+        {
+            var region = PolarVector.Region(5);
+            var p1 = new PolarVector(1, 0);
+            var w1 = PolarVector.WhereRegion(region, p1.angle);
+
+            var p2 = new PolarVector(1, region);
+            var w2 = PolarVector.WhereRegion(region, p2.angle);
+
+            var p3 = new PolarVector(1, 2 * region);
+            var w3 = PolarVector.WhereRegion(region, p3.angle);
+
+            Debug.Log(w1);
+            Debug.Log(w2);
+            Debug.Log(w3);
+
+            var angle = PolarVector.Angle(p1, p2);
+            var offset = PolarVector.WhereRegion(region, angle);
+            Debug.Log(angle);
+            Debug.Log(offset);
+
+            var area = PolarVector.Area(p1, p2);
+            Debug.Log(area);
+
+            Debug.Log("----");
+            angle = PolarVector.Angle(p1, p3);
+            offset = PolarVector.WhereRegion(region, angle);
+            Debug.Log(angle);
+            Debug.Log(offset);
+
+            area = PolarVector.Area(p1, p3);
+            Debug.Log(area);
+        }
+        #endregion
+    }
+}
---
+++ dev/NibaUnity2/Assets/Demo/RobotWar/Controller/Network/RobotWarClient.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using GameFramework.GameStructure;
+using System.Linq;
+using GameFramework.UI.Dialogs.Components;
+
+namespace RobotWar
+{
+    public class RobotWarClient : NetworkBehaviour
+    {
+        public override void OnStartLocalPlayer()
+        {
+            Debug.Log("OnStartLocalPlayer");
+            if (isLocalPlayer == false)
+            {
+                return;
+            }
+            CmdAddClient();
+        }
+
+        public void ServerAlert(string title, string msg)
+        {
+            foreach (var c in clients)
+            {
+                c.RpcAlert(title, msg);
+            }
+        }
+
+        #region client manager
+        public int playerId;
+        public static List<RobotWarClient> clients = new List<RobotWarClient>();
+        [Command]
+        void CmdAddClient()
+        {
+            var team = clients.Count;
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            var player = DataAlg.CreatePlayer(model.mapCtx, team, false);
+            playerId = player.Key;
+            clients.Add(this);
+            RpcSetClient(playerId);
+        }
+        #endregion
+        [ClientRpc]
+        void RpcSetClient(int playerId)
+        {
+            Debug.Log("RpcSetClient");
+            this.playerId = playerId;
+            var ctr = FindObjectOfType<MultiController>();
+            if (ctr != null)
+            {
+                ctr.client = this;
+            }
+        }
+        [Command]
+        public void CmdPassUnit(int playerId, string unit)
+        {
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            DataAlg.PassUnit(model.mapCtx, unit);
+            model.RequestSaveMap();
+        }
+        [Command]
+        public void CmdMoveUnit(int playerId, string unit, string pos)
+        {
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            var s = model.mapCtx.grids[model.mapCtx.unit2Grid[unit]].pos;
+            var e = model.mapCtx.grids[pos].pos;
+            var path = DataAlg.FindPath(model.mapCtx, s, e).Select(g => g.Key).ToArray();
+            DataAlg.MoveUnit(model.mapCtx, e, unit);
+            model.RequestSaveMap();
+            foreach (var c in clients)
+            {
+                c.RpcAnimateUnitMove(unit, path);
+            }
+        }
+        [Command]
+        public void CmdCancelMoveUnit(int playerId, string unit)
+        {
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            var pos = DataAlg.CancelMoveUnit(model.mapCtx, unit);
+            model.RequestSaveMap();
+
+            foreach (var c in clients)
+            {
+                c.RpcSetUnitPos(unit, pos);
+            }
+        }
+        [Command]
+        public void CmdPushTask(int playerId, string taskJson, bool isAttack)
+        {
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            var task = JsonUtility.FromJson<Task>(taskJson);
+            DataAlg.PushTask(model.mapCtx, task, isAttack);
+            model.RequestSaveMap();
+        }
+        [Command]
+        public void CmdSetUnitDirection(int playerId, string unit, Direction dir)
+        {
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            model.mapCtx.units[unit].dir = dir;
+            model.RequestSaveMap();
+            foreach (var c in clients)
+            {
+                c.RpcAnimateUnitDirection(unit, dir);
+            }
+        }
+        [ClientRpc]
+        public void RpcAlert(string title, string msg)
+        {
+            ModelController.Alarm(DialogInstance.DialogButtonsType.Ok, title, msg, null);
+        }
+        [ClientRpc]
+        void RpcAnimateUnitDirection(string unit, Direction dir)
+        {
+
+        }
+        [ClientRpc]
+        void RpcAnimateUnitMove(string unit, string[] path)
+        {
+            var view = FindObjectOfType<View>();
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            var grids = path.Select(k => model.mapCtx.grids[k]).ToList();
+            view.StartCoroutine(view.AnimateUnitMove(unit, grids));
+        }
+        [ClientRpc]
+        void RpcSetUnitPos(string unit, string pos)
+        {
+            var view = FindObjectOfType<View>();
+            var model = GameManager.Instance.gameObject.GetComponent<Model>();
+            view.SetUnitPos(unit, model.mapCtx.grids[pos]);
+        }
+    }
+}
