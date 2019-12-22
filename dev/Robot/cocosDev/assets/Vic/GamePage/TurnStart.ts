a3a0b82e31a3d2a6fbd8f036d02a1d1bcb717e05
vicyu <finalsteve@hotmail.com>
2019-12-22 17:07:52 +0800

    no message

---
+++ dev/Robot/cocosDev/assets/Vic/GamePage/TurnStart.ts
@@ -0,0 +1,33 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    content: cc.Label = null;
+
+    @property(cc.Sprite)
+    backColor:cc.Sprite = null;
+
+    setContent(content:string){
+        this.content.string = content;
+    }
+
+    setPlayer(isPlayer:boolean){
+        this.backColor.node.color = isPlayer ? cc.Color.BLUE : cc.Color.RED;
+    }
+
+    end(){
+        this.node.active = false;
+    }
+}
