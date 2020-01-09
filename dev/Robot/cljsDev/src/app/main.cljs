866c089e64fc5c7edad8f6ed2142d878e7aea22b
HanYu1983 <hanyushome@gmail.com>
2020-01-09 13:43:45 +0800

    move files and refactor

--- dev/Robot/cljsDev/src/app/main.cljs
+++ dev/Robot/cljsDev/src/app/main.cljs
@@ -1,30 +1,8 @@
 (ns app.main
   (:require [clojure.core.async :as a])
-  (:require [clojure.set])
-  (:require-macros [app.gameplay.macros :as m])
-  (:require [app.gameplay.phase.playerTurn :refer [playerTurn]])
-  (:require [app.gameplay.phase.enemyTurn :refer [enemyTurn]])
-  (:require [app.gameplay.phase.common :refer [updateMap
-                                               updateUnits]]))
-
+  (:require [app.gameplay.core]))
 
 (def defaultModel {})
-
-(defn gameplayLoop [gameplayCtx inputCh outputCh]
-  (a/go-loop [gameplayCtx gameplayCtx]
-    (let [gameplayCtx (a/<! (playerTurn gameplayCtx nil inputCh outputCh))
-          enemies (->> (:players gameplayCtx)
-                       keys
-                       (filter #(not= :player %)))
-          enemyTurns (a/go-loop [gameplayCtx gameplayCtx
-                                 enemies enemies]
-                       (if (= (count enemies) 0)
-                         gameplayCtx
-                         (let [enemy (first enemies)
-                               gameplayCtx (a/<! (enemyTurn gameplayCtx enemy inputCh outputCh))]
-                           (recur gameplayCtx (rest enemies)))))]
-      (recur (a/<! enemyTurns)))))
-
 
 (defn mainLoop [ctx inputCh outputCh]
   (a/go
@@ -35,23 +13,7 @@
           (recur ctx)
 
           (= "startGameplay" cmd)
-          (let [data (a/<! (app.gameplay.data/loadData))
-                playmap (app.gameplay.map/generateMap 100 100
-                                             {:deepsea 0.3
-                                              :sea 0.3
-                                              :sand 0.3
-                                              :grass 0.3
-                                              :city 0.3
-                                              :tree 0.3
-                                              :award 0.1})
-                gameplayCtx (-> app.gameplay.gameplay/defaultGameplayModel
-                                (app.gameplay.gameplay/setData data)
-                                (app.gameplay.gameplay/setMap playmap))]
-
-            (a/<! (updateMap gameplayCtx (app.gameplay.gameplay/getLocalMap gameplayCtx nil) inputCh outputCh))
-            (a/<! (updateUnits gameplayCtx (app.gameplay.gameplay/getLocalUnits gameplayCtx nil nil) inputCh outputCh))
-
-            (merge ctx {:gameplay (a/<! (gameplayLoop gameplayCtx inputCh outputCh))}))
+          (a/<! (app.gameplay.core/startGameplay ctx inputCh outputCh))
 
           :else
           (recur ctx))))))
