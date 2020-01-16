876b9c04e81f2ec89beef7204493fd7d335a139a
HanYu1983 <hanyushome@gmail.com>
2020-01-16 15:45:20 +0800

    no message

--- dev/Robot/cljsDev/src/app/gameplay/unit.cljs
+++ dev/Robot/cljsDev/src/app/gameplay/unit.cljs
@@ -1,8 +1,10 @@
 (ns app.gameplay.unit
   (:require [app.gameplay.module]))
 
-(defn model [unit]
-  (app.gameplay.module/unitStateCreate app.gameplay.module/*module unit))
+(defn model [{:keys [key position] :as unit}]
+  (merge (app.gameplay.module/unitStateCreate app.gameplay.module/*module unit)
+         {:key (or key (gensym))
+          :position (or position [0 0])}))
 
 (defn getWeapons [ctx]
   (app.gameplay.module/unitStateGetWeapons app.gameplay.module/*module ctx))
