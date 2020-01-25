80595821ca1b61d6ae972a8c8330ac4e02c71c46
vicyu <finalsteve@hotmail.com>
2020-01-25 18:48:25 +0800

    no message

---
+++ dev/Robot/cocosDev/assets/Vic/AnimationLoop.ts
@@ -0,0 +1,21 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property, requireComponent} = cc._decorator;
+
+@ccclass
+@requireComponent(cc.Animation)
+export default class NewClass extends cc.Component {
+
+    start () {
+        this.node.getComponent(cc.Animation).play().wrapMode = cc.WrapMode.Loop;
+    }
+
+}
