2528c7544c6ab57337e647f630fc43a9d1e75e31
HanYu1983bb <hanyushome@gmail.com>
2020-05-27 11:04:59 +0800

    no message

--- dev/Robot/cljsDev/src/app/lobby/model.cljs
+++ dev/Robot/cljsDev/src/app/lobby/model.cljs
@@ -2,16 +2,34 @@
   (:require [cljs.reader])
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::robots (s/map-of keyword? keyword?))
-(s/def ::pilots (s/map-of keyword? keyword?))
-(s/def ::robotByPilot (s/map-of keyword? keyword?))
+(s/def ::key keyword?)
+(s/def ::robotKey keyword?)
+(s/def ::pilotKey keyword?)
+(s/def ::robots (s/map-of ::key ::robotKey))
+(s/def ::pilots (s/map-of ::key ::pilotKey))
+(s/def ::robotByPilot (s/map-of ::key ::key))
 (s/def ::money int?)
-(s/def ::model (s/keys :req-un [::robots ::pilots ::robotByPilot ::money]))
+
+(s/def ::weaponKey keyword?)
+; 機器上的武器(哪台機體/哪個武器)
+(s/def ::weaponUpgradeKey (s/tuple ::key ::weaponKey))
+; 武器升級
+(s/def ::weaponLevelByWeaponUpgradeKey (s/map-of ::weaponUpgradeKey int?))
+
+(s/def ::componentKey keyword?)
+; 購買的配件
+(s/def ::components (s/map-of ::key ::componentKey))
+; 安裝在機體上的配件
+(s/def ::robotByComponent (s/map-of ::key ::key))
+
+(s/def ::model (s/keys :req-un [::robots ::pilots ::robotByPilot ::money ::weaponLevelByWeaponUpgradeKey ::robotByComponent]))
 
 (def defaultLobbyModel {:robots {}
                         :pilots {}
                         :robotByPilot {}
-                        :money 0})
+                        :money 0
+                        :weaponLevelByWeaponUpgradeKey {}
+                        :robotByComponent {}})
 
 
 (defn load []
