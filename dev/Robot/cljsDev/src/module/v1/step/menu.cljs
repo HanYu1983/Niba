b07f53aa38c9d555da03a3b23a44e0ba6b3295f0
HanYu1983bb <hanyushome@gmail.com>
2020-05-29 14:47:00 +0800

    no message

--- dev/Robot/cljsDev/src/module/v1/step/menu.cljs
+++ dev/Robot/cljsDev/src/module/v1/step/menu.cljs
@@ -1,4 +1,5 @@
 (ns module.v1.step.menu
+  (:require [clojure.spec.alpha :as s])
   (:require [clojure.core.async :as a])
   (:require-macros [module.v1.core :as core])
   (:require [tool.menuCursor])
@@ -7,27 +8,13 @@
   (:require [module.v1.data :as data])
   (:require [module.v1.type :as type])
   (:require [module.v1.system.mapViewSystem :as mapViewSystem])
-  (:require [module.v1.system.cursorViewSystem :as cursorViewSystem])
-  (:require [module.v1.system.moveRangeViewSystem :as moveRangeViewSystem])
-  (:require [module.v1.system.attackRangeViewSystem :as attackRangeViewSystem])
-  (:require [module.v1.system.hitRateViewSystem :as hitRateViewSystem])
-  (:require [module.v1.system.battleMenuViewSystem :as battleMenuViewSystem])
   (:require [module.v1.system.menuCursorViewSystem :as menuCursorViewSystem])
-  (:require [module.v1.common :as common]))
+  (:require [module.v1.common :as common])
+  (:require [module.v1.system.core :as systemCore]))
 
-(core/defstate menu {:keys [menu data]}
-  {:nameCtx gameplayCtx
-   :initState {:menuCursor (tool.menuCursor/model menu)
-               :data data}
-   :initCtx nil}
-  (loop [gameplayCtx gameplayCtx]
-    (a/<! (common/paint nil (data/render gameplayCtx) inputCh outputCh))
-    (let [[cmd args :as evt] (a/<! inputCh)
-          gameplayCtx (-> gameplayCtx
-                          (data/handleTest evt)
-                          (mapViewSystem/handleMapView evt)
-                          (menuCursorViewSystem/handleMenuCursor evt))
-          state (-> gameplayCtx :fsm tool.fsm/load)]
+(defn handleCore [gameplayCtx inputCh outputCh [cmd args]]
+  (a/go
+    (let [state (-> gameplayCtx :fsm tool.fsm/load)]
       (cond
         (= "KEY_DOWN" cmd)
         (let [action (common/actions args)]
@@ -37,7 +24,29 @@
               [gameplayCtx select])
 
             :else
-            (recur gameplayCtx)))
+            gameplayCtx))
 
         :else
-        (recur gameplayCtx)))))
+        gameplayCtx))))
+
+(core/defstate menu {:keys [menu data]}
+  {:nameCtx gameplayCtx
+   :initState {:menuCursor (tool.menuCursor/model menu)
+               :data data}
+   :initCtx nil}
+  (loop [gameplayCtx gameplayCtx]
+    (a/<! (common/paint nil (data/render gameplayCtx) inputCh outputCh))
+    (let [[cmd args :as evt] (a/<! inputCh)
+          returnCtx (-> gameplayCtx
+                        (data/handleTest evt)
+                        (mapViewSystem/handleMapView evt)
+                        (menuCursorViewSystem/handleMenuCursor evt)
+                        (#(systemCore/asyncMapReturn handleCore % inputCh outputCh evt))
+                        (a/<!))]
+      (let [conform (s/conform ::type/returnCtx returnCtx)]
+        (if (= ::s/invalid conform)
+          (throw (js/Error. (s/explain-str ::type/returnCtx returnCtx)))
+          (let [[returnType _] conform]
+            (condp = returnType
+              :return returnCtx
+              (recur returnCtx))))))))
--- dev/Robot/cljsDev/src/module/v1/step/selectPosition.cljs
+++ dev/Robot/cljsDev/src/module/v1/step/selectPosition.cljs
@@ -1,4 +1,5 @@
 (ns module.v1.step.selectPosition
+  (:require [clojure.spec.alpha :as s])
   (:require [clojure.core.async :as a])
   (:require-macros [module.v1.core :as core])
   (:require [tool.menuCursor])
@@ -7,7 +8,27 @@
   (:require [module.v1.common :as common])
   (:require [module.v1.system.mapViewSystem :as mapViewSystem])
   (:require [module.v1.system.cursorViewSystem :as cursorViewSystem])
-  (:require [module.v1.system.moveRangeViewSystem :as moveRangeViewSystem]))
+  (:require [module.v1.system.moveRangeViewSystem :as moveRangeViewSystem])
+  (:require [module.v1.system.core :as systemCore]))
+
+
+(defn handleCore [gameplayCtx inputCh outputCh [cmd args]]
+  (a/go
+    (cond
+      (= "KEY_DOWN" cmd)
+      (let [action (common/actions args)]
+        (cond
+          (= :cancel action)
+          [gameplayCtx false]
+
+          (= :enter action)
+          [gameplayCtx true]
+
+          :else
+          gameplayCtx))
+
+      :else
+      gameplayCtx)))
 
 (core/defstate selectPosition _
   {:nameCtx gameplayCtx
@@ -16,23 +37,17 @@
   (loop [gameplayCtx gameplayCtx]
     (a/<! (common/paint nil (data/render gameplayCtx) inputCh outputCh))
     (let [[cmd args :as evt] (a/<! inputCh)
-          gameplayCtx (-> gameplayCtx
+          returnCtx (-> gameplayCtx
                           (data/handleTest evt)
                           (mapViewSystem/handleMapView evt)
                           (cursorViewSystem/handleCursorView evt)
-                          (moveRangeViewSystem/handleMoveRangeView evt))]
-      (cond
-        (= "KEY_DOWN" cmd)
-        (let [action (common/actions args)]
-          (cond
-            (= :cancel action)
-            [gameplayCtx false]
-
-            (= :enter action)
-            [gameplayCtx true]
-
-            :else
-            (recur gameplayCtx)))
-
-        :else
-        (recur gameplayCtx)))))
+                          (moveRangeViewSystem/handleMoveRangeView evt)
+                          (#(systemCore/asyncMapReturn handleCore % inputCh outputCh evt))
+                          (a/<!))]
+      (let [conform (s/conform ::type/returnCtx returnCtx)]
+        (if (= ::s/invalid conform)
+          (throw (js/Error. (s/explain-str ::type/returnCtx returnCtx)))
+          (let [[returnType _] conform]
+            (condp = returnType
+              :return returnCtx
+              (recur returnCtx))))))))
