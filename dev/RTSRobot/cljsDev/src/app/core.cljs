b411e6fde2087600355c13126f6eaa8380036ef3
HanYu1983 <hanyushome@gmail.com>
2020-07-04 12:33:11 +0800

    no message

--- dev/RTSRobot/cljsDev/src/app/core.cljs
+++ dev/RTSRobot/cljsDev/src/app/core.cljs
@@ -1,69 +1,63 @@
 (ns app.core
-  (:require ["matter-js" :as matter :refer [Engine Render World Bodies Body]]
+  (:require ["planck-js" :as pl]
             [clojure.spec.alpha :as s]
             [clojure.core.async :as a]))
 
 (s/check-asserts true)
 
-(s/def ::id string?)
-(s/def ::body (fn [obj]
-                (and (.hasOwnProperty obj "mass")
-                     (.hasOwnProperty obj "position"))))
-(s/def ::force (fn [obj]
-                 (and (.hasOwnProperty obj "x")
-                      (.hasOwnProperty obj "y"))))
-(s/def ::entity (s/keys :req-un [::id ::body ::force]))
-(s/def ::entities (s/coll-of ::entity))
-
-(s/def ::matter (s/keys :req-un [::engine ::render]))
-(s/def ::gameplay (s/keys :req-un [::matter ::entities]))
-
-(def gameplay {:matter (let [world (.create World (clj->js {:gravity {:x 0 :y 0}}))
-                             engine (.create Engine (clj->js {:world world}))
-                             render (.create Render (clj->js {:element js/document.body
-                                                              :engine engine}))
-                             _ (.run Render render)]
-                         {:engine engine
-                          :render render})
-               :entities []})
-
-(defn add-entity [gameplay entity]
-  (s/assert ::gameplay gameplay)
-  (s/assert
-   ::gameplay
-   (do
-     (when-let [body (:body entity)]
-       (s/assert ::body body)
-       (.add World
-             (-> gameplay :matter :engine (.-world))
-             (array body)))
-     #_(update gameplay :entities #(conj % entity))
-
-     gameplay)))
-
-(defn update-engine! [gameplay t]
-  (s/assert ::gameplay gameplay)
-  (.update Engine (-> gameplay :matter :engine) t)
-  gameplay)
-
-
-(defn forceSystem [gameplay]
-  (s/assert ::gameplay gameplay)
-  (doseq [entity (:entities gameplay)
-          :when (s/valid? (s/keys :req-un [::body ::velocity]) entity)
-          :let [{:keys [body velocity]} entity]]
-    (let [_ (.applyForce Body body (.-position body) velocity)]))
-  gameplay)
-
 (defn main []
-  (a/go
-    (let [boxA {:id "ac"
-                :body (.rectangle Bodies 400 200 80 80)}
-          gameplay (add-entity gameplay boxA)]
-      (loop [gameplay gameplay]
-        (let [_ (update-engine! gameplay 33)
-              _ (a/<! (a/timeout 33))]
-          (recur gameplay))))))
+  (let [world (pl/World. (js-obj))
+        fix (-> (.createDynamicBody world (js-obj "position" (pl/Vec2. 0 0)
+                                                  "userData" (js-obj "id" 20)))
+                (.createFixture (pl/Circle. (pl/Vec2. 0 0) 1) (js-obj "friction" 0.1
+                                                                      "restitution" 0.99
+                                                                      "density" 1
+                                                                      "userData" "ball")))
+        _ (-> (.createBody world (js-obj "position" (pl/Vec2 10 10)
+                                         "angle" 1))
+              (.createFixture (pl/Box. 1 1)))
+        ; render
+        _ (loop [body (.getBodyList world)]
+            (when body
+              (loop [fixture (.getFixtureList body)]
+                (when fixture
+                  (let [shape (.getShape fixture)
+                        body (.getBody fixture)
+                        tx (.getTransform body)
+                        _ (condp = (.getType shape)
+                            "circle"
+                            0
+                            "polygon"
+                            (let [worldVertices (-> shape
+                                                    (.-m_vertices)
+                                                    (.map (pl/Transform.mulFn tx)))
+                                  _ (js/console.log worldVertices)])
+                            0)])
+                  (js/console.log "store:" fixture)
+                  (recur (.getNext fixture))))
+              (recur (.getNext body))))
+
+        ; apply force
+        body (.getBody fix)
+        _ (.applyForce body (pl/Vec2. 1 1) (pl/Vec2. 0 0) true)
+        ; apply force
+        _ (.step world 10)
+        shape (.getShape fix)
+        _ (js/console.log body)
+        _ (js/console.log shape (.getCenter shape) (.getWorldPoint body (.getCenter shape)))
+        ; query aabb
+        _ (.queryAABB world
+                      (pl/AABB. (pl/Vec2. 0 0) (pl/Vec2. 5 5))
+                      (fn [fixture]
+                        (js/console.log "find" fixture (.getUserData fixture))
+                        true))
+        ; change position
+        _ (.setPosition body (pl/Vec2 10 10))
+        ; query aabb again
+        _ (.queryAABB world
+                      (pl/AABB. (pl/Vec2. 0 0) (pl/Vec2. 15 15))
+                      (fn [fixture]
+                        (js/console.log "find" fixture (.getUserData fixture))
+                        true))]))
 
 (main)
-
