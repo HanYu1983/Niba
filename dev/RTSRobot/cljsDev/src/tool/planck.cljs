9f8ce5f0f525a5aa141a260828db18cfd564b2e7
HanYu1983 <hanyushome@gmail.com>
2020-09-26 14:17:30 +0800

    no message

--- dev/RTSRobot/cljsDev/src/tool/planck.cljs
+++ dev/RTSRobot/cljsDev/src/tool/planck.cljs
@@ -24,15 +24,74 @@
         _ (when (> (count hold-ids) 0)
             (on-update hold-ids))]))
 
+(s/def ::vec2 (s/tuple number? number?))
 
 (s/def ::world #(instance? pl/World %))
 (s/def ::body #(instance? pl/Body %))
-(s/def ::velocity (s/tuple number? number?))
-(s/def ::entity (s/keys :req-un [::id]
-                        :opt-un [::velocity]))
+(s/def ::fixture #(instance? pl/Fixture %))
+
+(s/def ::density number?)
+(s/def ::filterCategoryBits number?)
+(s/def ::filterGroupIndex number?)
+(s/def ::filterMaskBits number?)
+(s/def ::friction number?)
+(s/def ::isSensor boolean?)
+(s/def ::restitution number?)
+(s/def ::shape-def (s/or :rect (s/tuple #{:rect} ::vec2 ::vec2)
+                         :arc (s/tuple #{:arc} number? number? number?)
+                         :circle (s/tuple #{:circle} number?)
+                         :polygon (s/tuple #{:polygon} (s/coll-of ::vec2))))
+(s/def ::userData any?)
+(s/def ::fixture-def (s/keys :opt-un [::density
+                                      ::filterCategoryBits
+                                      ::filterGroupIndex
+                                      ::filterMaskBits
+                                      ::friction
+                                      ::isSensor
+                                      ::restitution
+                                      ::shape-def
+                                      ::filterMaskBits
+                                      ::userData]))
+
+(s/def ::active boolean?)
+(s/def ::allowSleep boolean?)
+(s/def ::angle number?)
+(s/def ::angularDamping number?)
+(s/def ::angularVelocity number?)
+(s/def ::awake boolean?)
+(s/def ::bullet boolean?)
+(s/def ::fixedRotation boolean?)
+(s/def ::gravityScale number?)
+(s/def ::linearDamping number?)
+(s/def ::linearVelocity ::vec2)
+(s/def ::position ::vec2)
+(s/def ::type string?)
+(s/def ::fixtures-def (s/coll-of ::fixture-def))
+(s/def ::body-def (s/keys :opt-un [::active
+                                   ::allowSleep
+                                   ::angle
+                                   ::angularDamping
+                                   ::angularVelocity
+                                   ::awake
+                                   ::bullet
+                                   ::fixedRotation
+                                   ::gravityScale
+                                   ::linearDamping
+                                   ::linearVelocity
+                                   ::position
+                                   ::type
+                                   ::userData
+                                   ::fixture-def]))
+
+(s/def ::entity (s/keys :req-un [::body-def]))
+
+(defn pl-vector [[x y]]
+  (pl/Vec2 x y))
 
 (defn reduce-bodies [planck f ctx]
   (s/assert ::world planck)
+  (s/assert fn? f)
+  (s/assert any? ctx)
   (let [ctx (loop [body (.getBodyList planck)
                    ctx ctx]
               (if body
@@ -42,6 +101,8 @@
 
 (defn reduce-fixtures [body f ctx]
   (s/assert ::body body)
+  (s/assert fn? f)
+  (s/assert any? ctx)
   (let [ctx (loop [fix (.getFixtureList body)
                    ctx ctx]
               (if fix
@@ -49,8 +110,35 @@
                 ctx))]
     ctx))
 
-(defn pl-vector [[x y]]
-  (pl/Vec2 x y))
+(defn reduce-query-fixtures [planck p1 p2 f ctx]
+  (s/assert ::world planck)
+  (s/assert ::vec2 p1)
+  (s/assert ::vec2 p2)
+  (s/assert fn? f)
+  (s/assert any? ctx)
+  (let [atom-selected (atom ctx)
+        _ (.queryAABB planck
+                      (pl/AABB. (pl-vector p1)
+                                (pl-vector p2))
+                      (fn [fixture]
+                        (swap! atom-selected #(f % fixture))
+                        true))]
+    @atom-selected))
+
+(defn on! [planck evt-name on-event]
+  (s/assert ::world planck)
+  (s/assert #{:begin-contact :end-contact :pre-solve :post-solve :remove-joint :remove-fixture :remove-body} evt-name)
+  (s/assert fn? on-event)
+  (let [_ (.on planck (str evt-name)
+               (fn [e]
+                 (let [a (-> e .getFixtureA .getUserData)
+                       b (-> e .getFixtureB .getUserData)]
+                   (on-event a b))))]))
+
+(defn step! [planck t]
+  (s/assert ::world planck)
+  (s/assert number? t)
+  (.step planck t))
 
 (defn update! [planck old-entites now-entites]
   (s/assert ::world planck)
@@ -60,18 +148,26 @@
                    (let [entities (->> created-ids
                                        (map now-entites))
                          _ (doseq [entity entities]
-                             (let [body-def (:body entity)
-                                   body (-> planck
-                                            (.createDynamicBody (js-obj "position" (or (pl-vector (:position body-def))
-                                                                                       (pl/Vec2 0 0))
-                                                                        "userData" (:id entity))))
-                                   fixture-defs (:fixtures body-def)
+                             (let [; create body
+                                   body-def (:body-def entity)
+                                   body-def (clj->js (dissoc body-def :fixtures-def))
+                                   _ (when (:position body-def)
+                                       (set! (.-position body-def) (pl-vector (:position body-def))))
+                                   _ (when (:linearVelocity body-def)
+                                       (set! (.-linearVelocity body-def) (pl-vector (:linearVelocity body-def))))
+                                   _ (js/console.log body-def)
+                                   body (.createBody planck body-def)
+
+                                   ; create fixture
+                                   fixture-defs (:fixtures-def body-def)
                                    _ (doseq [fixture-def fixture-defs]
-                                       (.createFixture body
-                                                       (pl/Polygon (array (pl/Vec2 -2 -1)
-                                                                          (pl/Vec2 2 0)
-                                                                          (pl/Vec2 -2 1)))
-                                                       (js-obj "density" 1)))]))]))
+                                       (let [fixture-def (clj->js (dissoc fixture-def :shape-def))
+                                             _ (js/console.log fixture-def)]
+                                         (.createFixture body
+                                                         (pl/Polygon (array (pl/Vec2 -2 -1)
+                                                                            (pl/Vec2 2 0)
+                                                                            (pl/Vec2 -2 1)))
+                                                         fixture-def)))]))]))
                  (fn [removed-ids]
                    (let [_ (reduce-bodies planck (fn [_ body]
                                                    (when (removed-ids (-> body .getUserData))
@@ -80,9 +176,17 @@
                  (fn [hold-ids]
                    (let [_ (reduce-bodies planck (fn [_ body]
                                                    (when (hold-ids (-> body .getUserData))
-                                                     (let [entity (now-entites (-> body .getUserData))
-                                                           _ (when (:velocity entity)
-                                                               (.setLinearVelocity body (pl-vector (:velocity entity))))])))
+                                                     (let [old-entity (old-entites (-> body .getUserData))
+                                                           entity (now-entites (-> body .getUserData))
+                                                           _ (when (not= (:body-def old-entity) (:body-def now-entites))
+                                                               (when (not= (-> old-entity :body-def :linearVelocity) (-> entity :body-def :linearVelocity))
+                                                                 (.setLinearVelocity body (pl-vector (:velocity entity))))
+                                                               (when (not= (-> old-entity :body-def :active) (-> entity :body-def :active))
+                                                                 (.setActive body (-> entity :body-def :active)))
+                                                               (when (not= (-> old-entity :body-def :bullet) (-> entity :body-def :bullet))
+                                                                 (.setBullet body (-> entity :body-def :bullet)))
+                                                               (when (not= (-> old-entity :body-def :fixedRotation) (-> entity :body-def :fixedRotation))
+                                                                 (.setFixedRotation body (-> entity :body-def :fixedRotation))))])))
                                           0)]))
                  old-entites
                  now-entites))
