2a138fa62679b9c3899812dda64bd61def0e3a80
HanYu1983 <hanyushome@gmail.com>
2020-10-10 12:25:23 +0800

    no message

---
+++ dev/RTSRobot/cljsDev/public/js/app4.js
@@ -0,0 +1,1 @@
+SHADOW_ENV.load({}, ["tool.stock.spec.js","tool.stock.tool.js","tool.stock.drawer.js","tool.stock.formula.js","app4.core.js","shadow.module.app4.append.js"]);
--- dev/RTSRobot/cljsDev/src/app4/core.cljs
+++ dev/RTSRobot/cljsDev/src/app4/core.cljs
@@ -1,39 +1,12 @@
 (ns app4.core
-  (:require ["planck-js" :as pl]
-            [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s])
+  (:require [tool.stock.drawer]
+            [tool.stock.tool]
+            [tool.stock.formula]))
 
-(s/def ::world #(instance? pl/World %))
-(s/def ::body #(instance? pl/Body %))
-(s/def ::fixture #(instance? pl/Fixture %))
+(s/check-asserts true)
+(tool.stock.tool/test-it)
+(println (seq? nil))
+(println (cons 0 nil))
 
-(defn create-world [world-def]
-  (let [world (pl/World. world-def)]
-    world))
-
-(defn create-shape [shape-def])
-
-(defn create-body [world body-def]
-  (let [body (.createBody world body-def)
-        _ (.forEach (.-fixtures body-def)
-                    (fn [fixture-def]
-                      (let [shape-def (.-shape fixture-def)
-                            _ (.createFixture body
-                                              (create-shape shape-def)
-                                              fixture-def)])))
-        _ (.setUserData body body-def)]))
-
-(defn reduce-bodies [planck f ctx]
-  (s/assert ::world planck)
-  (s/assert fn? f)
-  (s/assert any? ctx)
-  (let [ctx (loop [body (.getBodyList planck)
-                   ctx ctx]
-              (if body
-                (recur (.getNext body) (f ctx (s/assert ::body body)))
-                ctx))]
-    ctx))
-
-(defn get-world-memonto [world]
-  (let [_ (reduce-bodies world (fn [ctx body]
-                                 (.push ctx (js-obj "userData" (.getUserData body))))
-                         (array))]))
+(println (take 5 nil))
