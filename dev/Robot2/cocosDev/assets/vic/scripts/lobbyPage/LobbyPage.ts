5e9758cfe531228fab685032d26e04632d8478f1
余漢平 <yuhanping@wangyuan.com>
2020-12-22 10:05:38 +0800

    no message

--- dev/Robot2/cocosDev/assets/vic/scripts/lobbyPage/LobbyPage.ts
+++ dev/Robot2/cocosDev/assets/vic/scripts/lobbyPage/LobbyPage.ts
@@ -5,11 +5,10 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator } from 'cc';
 import { GameInst } from '../GameInst';
 import * as ModelType from '../../../han/types'
-import { Instant } from '../lib/instanceViewer/Instant';
-const { ccclass, property } = _decorator;
+const { ccclass } = _decorator;
 
 @ccclass('LobbyPage')
 export class LobbyPage extends GameInst {
--- dev/Robot2/cocosDev/assets/vic/scripts/mainPage/MainPage.ts
+++ dev/Robot2/cocosDev/assets/vic/scripts/mainPage/MainPage.ts
@@ -5,11 +5,10 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator } from 'cc';
 import { GameInst } from '../GameInst';
-import { Instant } from '../lib/instanceViewer/Instant';
 import * as ModelType from '../../../han/types'
-const { ccclass, property } = _decorator;
+const { ccclass } = _decorator;
 
 @ccclass('MainPage')
 export class MainPage extends GameInst {
