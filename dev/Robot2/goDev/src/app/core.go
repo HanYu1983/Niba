4c2c483781ba635bb7f07879a8bb108c85616530
HanYu1983 <hanyushome@gmail.com>
2020-12-25 16:30:03 +0800

    no message

--- dev/Robot2/goDev/src/app/core.go
+++ dev/Robot2/goDev/src/app/core.go
@@ -40,8 +40,12 @@ func Render(ctx uidata.UI) {
 	ctx.Info.Money = model.QueryMoney()
 	ctx.Info.Robots = model.QueryRobots()
 	ctx.Info.Pilots = model.QueryPilots()
+	ctx.Info.Weapons = model.QueryWeapons()
+	ctx.Info.Components = model.QueryComponents()
 	ctx.Info.CanBuyRobots, _ = model.QueryRobotCanBuy()
 	ctx.Info.CanBuyPilots, _ = model.QueryPilotCanBuy()
+	ctx.Info.CanBuyWeapons, _ = model.QueryWeaponCanBuy()
+	ctx.Info.CanBuyComponents, _ = model.QueryComponentCanBuy()
 	view.Render(ctx)
 }
 
