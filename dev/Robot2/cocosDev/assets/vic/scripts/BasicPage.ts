73949c24f911e3b3c9eb78fb91b0d4761d7c24f1
余漢平 <yuhanping@wangyuan.com>
2020-12-29 11:04:21 +0800

    no message

--- dev/Robot2/cocosDev/assets/vic/scripts/BasicPage.ts
+++ dev/Robot2/cocosDev/assets/vic/scripts/BasicPage.ts
@@ -5,27 +5,26 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, CCInteger, Enum } from 'cc';
 import { Instant } from './lib/instanceViewer/Instant';
 import * as ModelType from '../../han/types'
 const { ccclass, property } = _decorator;
 
-@ccclass('GameInst')
-export class GameInst extends Instant {
+@ccclass('BasicPage')
+export class BasicPage extends Instant {
+
+    @property ({type:Enum(ModelType.Page)})
+    pageId:ModelType.Page = null;
 
     protected checkData(data:any):any{
         const content = {
-            Active: data.Actives[this.doCheckPage()],
-            Menus: data.Menus[this.doCheckPage()],
-            Focus: data.Focus[this.doCheckPage()],
+            Active: data.Actives[this.pageId],
+            Menus: data.Menus[this.pageId],
+            Focus: data.Focus[this.pageId],
         }
         if (content.Active) {
             return content;
         }
         return null;
-    }
-
-    protected doCheckPage():ModelType.Page{
-        return ModelType.Page.Start;
     }
 }
