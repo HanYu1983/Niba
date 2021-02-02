b89a2fd4411e37aa0e6dfdafcbae7c96a8a25824
HanYu1983 <hanyushome@gmail.com>
2021-02-02 11:58:32 +0800

    no message

--- dev/Robot2/goDev/src/getLib.bat
+++ dev/Robot2/goDev/src/getLib.bat
@@ -1,3 +1,5 @@
 SET GOPATH=D:\Documents\git\Niba\dev\Robot2\goDev\
 go get github.com/gopherjs/gopherjs
 go get github.com/cheekybits/genny/generic
+go get github.com/muesli/clusters
+go get github.com/muesli/kmeans
