1adacf30c905ff13126c66fb235199729934930b
HanYu1983 <hanyushome@gmail.com>
2021-02-17 15:50:13 +0800

    no message

--- dev/Robot2/goDev/src/app/model/v1/GenerateLevelByGeneticAlgo_test.go
+++ dev/Robot2/goDev/src/app/model/v1/GenerateLevelByGeneticAlgo_test.go
@@ -44,37 +44,4 @@ func TestGenerateLevelByGeneticAlgo(t *testing.T) {
 	if float64(countOfGaiteSea)/float64(len(ctx.App.Gameplay.Robots)) < 0.5 {
 		t.Fatalf("GaiteSea必須佔了一半以上")
 	}
-
-	t.Error("xx")
-}
-
-func TestGenerateLevelByGeneticAlgo2(t *testing.T) {
-	tempMap, err := helper.GenerateMap(helper.GenerateMapConfigIsland, 0, 0, 1, 25, 25, 0, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	model := DefaultModel
-	model.App.Gameplay.Map = tempMap
-	ctx, err := GenerateLevelByGeneticAlgo(model, "0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	countOfGaiteSea := 0
-	for _, robot := range ctx.App.Gameplay.Robots {
-		pos := ctx.App.Gameplay.Positions[robot.ID]
-		robotProto, err := data.TryGetStringRobotProto(data.GameData.Robot, robot.ProtoID)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if robotProto.ID == "gaite_sea" {
-			countOfGaiteSea++
-		}
-		fmt.Printf("robotProtoID(%v) pos(%v) suit(%v)\n", robot.ProtoID, pos, robotProto.Suitability)
-	}
-	fmt.Printf("countOfGaiteSea(%v/%v)\n", countOfGaiteSea, len(ctx.App.Gameplay.Robots))
-
-	if float64(countOfGaiteSea)/float64(len(ctx.App.Gameplay.Robots)) < 0.5 {
-		t.Fatalf("GaiteSea必須佔了一半以上")
-	}
-
 }
