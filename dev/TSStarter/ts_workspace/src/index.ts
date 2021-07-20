cda6ec474309829012b3df426c9a151134fd333e
hanyu <hanyushome@gmail.com>
2021-07-20 12:51:44 +0800

    add TSStarter

---
+++ dev/TSStarter/ts_workspace/src/index.ts
@@ -0,0 +1,11 @@
+import { double } from "./lib/number.js"
+
+
+const a: number | null = 2;
+
+const b = 2 + a
+const c = a + double(b)
+console.log(c)
+console.log("aa")
+console.log("xx")
+console.log("aa")
