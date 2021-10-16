35fd99811db0159fff5d987344d20426a80174ff
HanYu1983 <hanyushome@gmail.com>
2021-10-16 10:19:19 +0800

    no message

---
+++ dev/GundamCard/ts_workspace/src/model/debug/testPhase.ts
@@ -0,0 +1,79 @@
+import { mapCard, moveCard, createCard } from "../../tool/table";
+import {
+  Context,
+  Action,
+  Payment,
+  Effect,
+  defaultContext,
+} from "../../tool/types";
+import { askPlayerG, cardPositionID, onEffectCompleted } from "../alg";
+import { queryAction } from "../alg/queryAction";
+import { applyAction } from "../alg/applyAction";
+import { checkPayment } from "../alg/checkPayment";
+import { rootApp } from "../../tool/firebase";
+import { PlayerA, PlayerB } from "../../app/context";
+import { askCardColor } from "../alg";
+
+export function testPhase() {
+  let ctx: Context = {
+    ...defaultContext,
+    gameState: {
+      ...defaultContext.gameState,
+      phase: ["draw", "before"],
+      activePlayerID: PlayerA,
+    },
+  };
+  console.log("A宣告到下一步");
+  ctx = applyAction(ctx, PlayerA, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerA,
+  });
+  if ((ctx.gameState.playerState[PlayerA]?.confirmPhase || false) != true) {
+    throw new Error("PlayerA必須確認結束");
+  }
+  if ((ctx.gameState.playerState[PlayerB]?.confirmPhase || false) != false) {
+    throw new Error("PlayerB必須沒有結束");
+  }
+  console.log("B宣告到下一步");
+  ctx = applyAction(ctx, PlayerB, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerB,
+  });
+  if (ctx.gameState.phase[1] != "effect") {
+    throw new Error("必須到規定效果");
+  }
+  if ((ctx.gameState.playerState[PlayerA]?.confirmPhase || false) != false) {
+    throw new Error("PlayerA的確認狀態必須被清空");
+  }
+  if ((ctx.gameState.playerState[PlayerB]?.confirmPhase || false) != false) {
+    throw new Error("PlayerB的確認狀態必須被清空");
+  }
+  if (ctx.gameState.phase[1] != "effect") {
+    throw new Error("必須到規定效果");
+  }
+  console.log("A完成規定效果");
+  ctx = applyAction(ctx, PlayerA, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerA,
+  });
+  if (ctx.gameState.phase[1] != "after") {
+    throw new Error("必須到after");
+  }
+  console.log("現在到了抽牌階段規定效果後的自由時間");
+  console.log("A宣告到下一步");
+  ctx = applyAction(ctx, PlayerA, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerA,
+  });
+  console.log("B宣告到下一步");
+  ctx = applyAction(ctx, PlayerB, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerB,
+  });
+  if (ctx.gameState.phase[0] != "set") {
+    throw new Error("必須到設置階段");
+  }
+  if (ctx.gameState.phase[1] != "before") {
+    throw new Error("必須到設置階段的自由時間");
+  }
+}
---
+++ dev/GundamCard/ts_workspace/src/model/debug/testPlayG.ts
@@ -0,0 +1,67 @@
+import { mapCard, moveCard, createCard } from "../../tool/table";
+import {
+  Context,
+  Action,
+  Payment,
+  Effect,
+  defaultContext,
+} from "../../tool/types";
+import { askPlayerG, cardPositionID, onEffectCompleted } from "../alg";
+import { queryAction } from "../alg/queryAction";
+import { applyAction } from "../alg/applyAction";
+import { checkPayment } from "../alg/checkPayment";
+import { rootApp } from "../../tool/firebase";
+import { PlayerA, PlayerB } from "../../app/context";
+import { askCardColor } from "../alg";
+
+export function testPlayG() {
+  let ctx: Context = {
+    ...defaultContext,
+    gameState: {
+      ...defaultContext.gameState,
+      table: createCard(
+        defaultContext.gameState.table,
+        PlayerA,
+        cardPositionID({ playerID: PlayerA, where: "hand" }),
+        ["179030_11E_G_RD021N_red"]
+      ),
+    },
+  };
+  const actions = queryAction(ctx, PlayerA);
+  if (actions.length == 0) {
+    throw new Error("必須有出牌動作");
+  }
+  if (actions[0].id != "PlayCardAction") {
+    throw new Error("動作必須是PlayCardAction");
+  }
+  console.log("A出G");
+  ctx = applyAction(ctx, PlayerA, actions[0]);
+  console.log("A放棄切入");
+  ctx = applyAction(ctx, PlayerA, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerA,
+  });
+  console.log("B放棄切入");
+  ctx = applyAction(ctx, PlayerB, {
+    id: "ConfirmPhaseAction",
+    playerID: PlayerB,
+  });
+  if (
+    (
+      ctx.gameState.table.cardStack[
+        cardPositionID({ playerID: PlayerA, where: "G" })
+      ] || []
+    ).length != 1
+  ) {
+    throw new Error("G必須在場上");
+  }
+  if (
+    (
+      ctx.gameState.table.cardStack[
+        cardPositionID({ playerID: PlayerA, where: "hand" })
+      ] || []
+    ).length != 0
+  ) {
+    throw new Error("手牌必須為0");
+  }
+}
