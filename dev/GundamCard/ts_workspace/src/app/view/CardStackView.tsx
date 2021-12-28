63ed304f34d63b2d5c6fbda2df66f8905ef7514f
hanyu <hanyushome@gmail.com>
2021-12-28 16:42:19 +0800

    no message

---
+++ dev/GundamCard/ts_workspace/src/app/view/CardStackView.tsx
@@ -0,0 +1,32 @@
+import React, { useContext, useMemo, HTMLAttributes } from "react";
+import { AppContext } from "../context";
+import { cardPositionID } from "../../model/alg/tool";
+import { CardPosition } from "../../tool/types";
+import { CardView } from "./CardView";
+
+const CARD_SIZE = 100;
+
+export const CardStackView = (props: { cardPosition: CardPosition }) => {
+  const appContext = useContext(AppContext);
+  if (appContext == null) {
+    return <div>AppContext not found</div>;
+  }
+  const cards = useMemo(() => {
+    return (
+      appContext.viewModel.model.gameState.table.cardStack[
+        cardPositionID(props.cardPosition)
+      ] || []
+    );
+  }, [
+    props.cardPosition,
+    appContext.viewModel.model.gameState.table.cardStack,
+  ]);
+  return (
+    <div style={{ border: "1px solid", height: CARD_SIZE, display: "flex" }}>
+      <div style={{ width: 100 }}></div>
+      {cards.map((card) => {
+        return <CardView key={card.id} cardID={card.id}></CardView>;
+      })}
+    </div>
+  );
+};
--- dev/GundamCard/ts_workspace/src/app/view/index.tsx
+++ dev/GundamCard/ts_workspace/src/app/view/index.tsx
@@ -7,74 +7,9 @@ import React, {
   useEffect,
 } from "react";
 import { AppContext } from "../context";
-import { cardPositionID } from "../../model/alg/tool";
-import { CardPosition } from "../../tool/types";
-import { askImgSrc } from "../../tool/data";
-import { Card } from "../../tool/table";
 import { PlayerA, PlayerB } from "../../tool/types";
-import { getCard } from "../../tool/table";
 import { OnEvent } from "../../tool/eventCenter";
-
-const CARD_SIZE = 100;
-
-export const CardView = (props: { cardID: string }) => {
-  const appContext = useContext(AppContext);
-  if (appContext == null) {
-    return <div>AppContext not found</div>;
-  }
-  const card = useMemo(() => {
-    return getCard(appContext.viewModel.model.gameState.table, props.cardID);
-  }, [props.cardID, appContext.viewModel.model.gameState.table]);
-  if (card == null) {
-    return <div>card({props.cardID}) not found</div>;
-  }
-  const render = useMemo(() => {
-    return (
-      <div
-        style={{
-          ...(card.tap ? { transform: "rotate(90deg)" } : null),
-          border: "2px solid black",
-          ...(appContext.viewModel.cardSelection[card.id]
-            ? { border: "2px solid red" }
-            : null),
-        }}
-        onClick={() => {
-          OnEvent.next({ id: "OnClickCardEvent", card: card });
-        }}
-      >
-        <img src={askImgSrc(card.protoID)} style={{ height: CARD_SIZE }}></img>
-      </div>
-    );
-  }, [card, appContext.viewModel.cardSelection]);
-  return <>{render}</>;
-};
-
-export const CardStackView = (
-  props: { cardPosition: CardPosition } & HTMLAttributes<unknown>
-) => {
-  const appContext = useContext(AppContext);
-  if (appContext == null) {
-    return <div>AppContext not found</div>;
-  }
-  const cards = useMemo(() => {
-    return (
-      appContext.viewModel.model.gameState.table.cardStack[
-        cardPositionID(props.cardPosition)
-      ] || []
-    );
-  }, [
-    props.cardPosition,
-    appContext.viewModel.model.gameState.table.cardStack,
-  ]);
-  return (
-    <div style={{ border: "1px solid", height: CARD_SIZE, display: "flex" }}>
-      <div style={{ width: 100 }}></div>
-      {cards.map((card) => {
-        return <CardView key={card.id} cardID={card.id}></CardView>;
-      })}
-    </div>
-  );
-};
+import { CardStackView } from "./CardStackView";
 
 export function View() {
   const appContext = useContext(AppContext);
