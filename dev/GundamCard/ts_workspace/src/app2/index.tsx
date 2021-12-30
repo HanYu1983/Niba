f26966243ec6934df11e68bf296e815d6fb91001
hanyu <hanyushome@gmail.com>
2021-12-30 08:31:54 +0800

    no message

---
+++ dev/GundamCard/ts_workspace/src/app2/index.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useContext } from "react";
+import ReactDOM from "react-dom";
+const App = (props: any) => {
+  return <div>app2</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
