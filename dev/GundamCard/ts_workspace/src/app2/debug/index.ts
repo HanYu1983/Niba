8409e080ed46984eeeb9a8d08debf9f64474d5c1
HanYu1983 <hanyushome@gmail.com>
2022-01-25 09:41:06 +0800

    no message

--- dev/GundamCard/ts_workspace/src/app2/debug/index.ts
+++ dev/GundamCard/ts_workspace/src/app2/debug/index.ts
@@ -36,17 +36,206 @@ import {
 } from "./testProto_179025_07D_U_RD156R_red";
 import { getTargetType } from "../tool/alg/helper";
 
+const jsonfp = require("jsonfp");
+
+export function testJsonfp() {
+  console.log(jsonfp);
+  jsonfp.init();
+  {
+    const ctx = {};
+    const ret = jsonfp.apply(
+      ctx,
+      [{ name: "w" }, { name: "g" }],
+      [
+        {
+          $name: { map: "$in.name" },
+          ret1: { "->": [5, { multiply: { clone: null } }] },
+          response: { "->": ["$name", { add: "gan!" }] },
+        },
+        // eval會把結果移到ctx中
+        {
+          $eval: {
+            $name: { map: "$in.name" },
+            response: { "->": ["$name", { add: "gan!" }] },
+          },
+        },
+      ]
+    );
+    console.log(ctx, ret);
+  }
+
+  {
+    var jsquare = {
+      "->": [{ random: null }, { multiply: { clone: null } }],
+    };
+    var jexpr = {
+      ret1: {
+        "->": [
+          // iterator的下一個值代表初值
+          [{ "stream/iterator": { start: 1, end: "$in" } }, 3],
+          { reduce: "add" },
+        ],
+      },
+      ret2: {
+        "->": [
+          [
+            {
+              "->": [
+                { "stream/iterator": { start: 1, end: "$in" } },
+                {
+                  "->": [
+                    {
+                      map: {
+                        "->": [
+                          [jsquare, jsquare],
+                          { reduce: "add" },
+                          { subtract: 1 },
+                          { "<=": 0 },
+                        ],
+                      },
+                    },
+                    { reduce: "add" },
+                  ],
+                },
+              ],
+            },
+            100,
+          ],
+          { reduce: "add" },
+        ],
+      },
+    };
+    const ctx = {};
+    // iterate 2000 times
+    const ret2 = jsonfp.apply(ctx, 10, jexpr);
+    console.log(ctx, ret2);
+  }
+
+  {
+    // 把輸入的值當成變量e, 再把e放到物件中的wow變數
+    const p = {
+      formula: {
+        var: "e",
+        expr: { wow: "e" },
+      },
+    };
+    const ctx = {};
+    const result = jsonfp.apply(ctx, { def: { pick: "name" } }, p);
+    console.log(ctx, result);
+  }
+  {
+    // merge == zip + merge
+    var list = [{ name: "John" }, { name: "Kate" }, { xx: 0 }];
+    var p = {
+      merge: [{ project: "coServ" }, { project: "JSON-fp" }, { xx2: 1 }],
+    };
+    var result = jsonfp.apply(list, p);
+    console.log(result);
+  }
+
+  {
+    var avgTemplate = {
+      formula: {
+        var: "@prop",
+        expr: {
+          "->": [
+            [
+              { "->": [{ map: { getter: "@prop" } }, { reduce: "add" }] },
+              { size: null },
+            ],
+            { reduce: "divide" },
+          ],
+        },
+      },
+    };
+
+    // now we'll use eval to perform the variable substitution
+    var program = {
+      $stat: {
+        eval: {
+          era: {
+            eval: {
+              "->": ["era", avgTemplate],
+            },
+          },
+          salary: {
+            eval: {
+              "->": ["salary", avgTemplate],
+            },
+          },
+        },
+      },
+      pitchers: {
+        filter: {
+          "->": [
+            [
+              { "->": [{ getter: "era" }, { "<": "$stat.era" }] },
+              { "->": [{ getter: "salary" }, { "<": "$stat.salary" }] },
+            ],
+            { reduce: "and" },
+          ],
+        },
+      },
+
+      $test_era: {
+        // convert在這種情況下等於eval
+        convert: {
+          var: { "@prop": "era" },
+          formula: avgTemplate,
+        },
+      },
+      $test_salary: {
+        // convert在這種情況下等於eval
+        convert: {
+          var: { "@prop": "salary" },
+          formula: avgTemplate,
+        },
+      },
+      test_pitchers: {
+        filter: {
+          "->": [
+            [
+              { "->": [{ getter: "era" }, { "<": "$test_era" }] },
+              { "->": [{ getter: "salary" }, { "<": "$test_salary" }] },
+            ],
+            { reduce: "and" },
+          ],
+        },
+      },
+    };
+
+    var pitchers = [
+      { name: "Ventura", era: 3.2, salary: 500500 },
+      { name: "Price", era: 3.26, salary: 19750000 },
+      { name: "Kershaw", era: 1.77, salary: 32517428 },
+      { name: "Gray", era: 3.08, salary: 505000 },
+      { name: "Liriano", era: 3.38, salary: 11666666 },
+      { name: "Hammel", era: 3.47, salary: 23500000 },
+      { name: "Lester", era: 2.46, salary: 20000000 },
+      { name: "Bumgarner", era: 2.98, salary: 6950000 },
+      { name: "Chen", era: 3.54, salary: 4750000 },
+      { name: "Norris", era: 3.65, salary: 8800000 },
+    ];
+
+    var ctx = {},
+      result = jsonfp.apply(ctx, pitchers, program);
+
+    console.log(ctx, result);
+  }
+}
+
 export function test() {
   [
-    testTargetType,
+    testJsonfp,
+    //testTargetType,
     // testDryRun,
     // testFlow1,
     // testFlow2,
     // testKaiSo1,
     // testClientCommand,
     // testClientCommand2,
-    testProto_179025_07D_U_RD156R_red,
-    testProto_179025_07D_U_RD156R_red2,
+    // testProto_179025_07D_U_RD156R_red,
+    // testProto_179025_07D_U_RD156R_red2,
   ].forEach((testF: Function) => {
     console.log(
       `================================================================`
