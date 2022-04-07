9b805c97d4cad9d12319125cc2cbeda4a672d984
HanYu1983 <hanyushome@gmail.com>
2022-04-07 18:12:18 +0800

    no message

--- dev/SanguoRich/haxeDev/src/Main.hx
+++ dev/SanguoRich/haxeDev/src/Main.hx
@@ -1,20 +1,21 @@
-package ;
+package;
 
 import model.DebugModel;
 import model.IModel;
 import view.MainView;
 import haxe.ui.HaxeUIApp;
+import model.ModelVer1;
 
 class Main {
+	public static var model:IModel;
 
-    public static var model:IModel;
+	public static function main() {
+		// model = new DebugModel();
+		model = new ModelVer1();
 
-    public static function main() {
-        model = new DebugModel();
-        
-        var app = new HaxeUIApp();
-        app.ready(function() {
-            app.addComponent(new MainView());
-        });
-    }
+		var app = new HaxeUIApp();
+		app.ready(function() {
+			app.addComponent(new MainView());
+		});
+	}
 }
---
+++ dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
@@ -0,0 +1,5 @@
+package model;
+
+import model.DebugModel;
+
+class ModelVer1 extends DebugModel {}
