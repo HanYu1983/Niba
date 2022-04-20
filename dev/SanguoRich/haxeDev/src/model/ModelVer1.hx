cbfe14f94cf45ffa88114dd50b6c5b1c82088a1b
HanYu1983 <hanyushome@gmail.com>
2022-04-20 22:10:18 +0800

    no message

--- dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
+++ dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
@@ -85,45 +85,97 @@ class ModelVer1 extends DebugModel {
 		cb();
 	}
 
-	override public function getTakeNegoPreview(playerId:Int, gridId:Int):NegoPreview {
+	override function getTakeNegoPreview(playerId:Int, gridId:Int):NegoPreview {
 		return {
 			p1ValidPeople: info.players[playerId].people,
 			p2ValidPeople: info.grids[gridId].people
 		};
 	}
 
-	override public function takeNegoOn(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int, cb:(gameInfo:GameInfo) -> Void) {
-		var info = gameInfo();
+	function getPeopleIterator():Array<People> {
+		var ret:Array<People> = [];
+		for (player in info.players) {
+			ret = ret.concat(player.people);
+		}
+		for (grid in info.grids) {
+			ret = ret.concat(grid.people);
+		}
+		return ret;
+	}
+
+	function getPeopleById(id:Int):People {
+		var find = getPeopleIterator().filter(p -> p.id == id);
+		if (find.length == 0) {
+			throw new haxe.Exception('people not found: ${id}');
+		}
+		return find[0];
+	}
+
+	function getNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
+		var fightPeople = [p1SelectId, p2SelectId].map(getPeopleById);
+		switch fightPeople {
+			case [p1, p2]:
+				return {
+					playerCost: {
+						id: playerId,
+						army: 10,
+						money: 10,
+						food: 10
+					},
+					peopleCost: {
+						id: p1.id,
+						energy: 10,
+					},
+					successRate: 0.7
+				};
+			default:
+				throw new haxe.Exception("fightPeople not right");
+		}
+	}
+
+	function applyNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
+		var negoCost = getNegoCost(playerId, gridId, p1SelectId, p2SelectId);
+		// TODO
+	}
+
+	override function takeNegoOn(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int, cb:(gameInfo:GameInfo) -> Void) {
+		var p1 = getPeopleById(p1SelectId);
+		var p2 = getPeopleById(p2SelectId);
+		var preResult = getPreResultOfNego(playerId, gridId, p1, p2);
+		applyNegoCost(playerId, gridId, p1SelectId, p2SelectId);
 		info.events = [
 			{
 				id: EventInfoID.NEGOTIATE_RESULT,
 				value: {
 					success: true,
-					people: PeopleGenerator.getInst().generate(),
-					energyBefore: 100,
-					energyAfter: 50,
-					armyBefore: 200,
-					armyAfter: 300,
-					moneyBefore: 200,
-					moneyAfter: 300,
-					foodBefore: 100,
-					foodAfter: 200
+					people: p1,
+					energyBefore: p1.energy,
+					energyAfter: preResult.energyAfter,
+					armyBefore: preResult.armyBefore,
+					armyAfter: preResult.armyAfter,
+					moneyBefore: preResult.moneyBefore,
+					moneyAfter: preResult.moneyAfter,
+					foodBefore: preResult.foodBefore,
+					foodAfter: preResult.foodAfter,
 				}
 			}
 		];
+		updateGameInfo();
 		cb(info);
 	}
 
 	override function getPreResultOfNego(playerId:Int, gridId:Int, people:People, invite:People):PreResultOnNego {
+		var player = info.players[playerId];
+		var negoCost = getNegoCost(playerId, gridId, people.id, invite.id);
 		return {
-			energyAfter: 1,
-			armyBefore: 2,
-			armyAfter: 3,
-			moneyBefore: 4,
-			moneyAfter: 5,
-			foodBefore: 6,
-			foodAfter: 7,
-			successRate: .8
+			energyAfter: people.energy - negoCost.peopleCost.energy,
+			armyBefore: Std.int(player.army),
+			armyAfter: Std.int(player.army - negoCost.playerCost.army),
+			moneyBefore: Std.int(player.money),
+			moneyAfter: Std.int(player.money - negoCost.playerCost.money),
+			foodBefore: Std.int(player.food),
+			foodAfter: Std.int(player.food - negoCost.playerCost.food),
+			successRate: negoCost.successRate
 		};
 	}
 
