fae20f0af7b6b5d2b7767e612ed48381aac2b2fa
HanYu1983 <hanyushome@gmail.com>
2022-04-21 19:31:25 +0800

    no message

--- dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
+++ dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
@@ -27,7 +27,7 @@ var defaultGameInfoBackground:GameInfoBackground = {
 // 請參照html5.hxml
 // https://haxe.org/manual/cr-null-safety.html
 class ModelVer1 extends DebugModel {
-	var background = defaultGameInfoBackground;
+	final background = defaultGameInfoBackground;
 
 	function updateGameInfo() {
 		info.isPlayerTurn = true; // background.currentPlayerId == 0;
@@ -51,11 +51,15 @@ class ModelVer1 extends DebugModel {
 	}
 
 	override function playerDice(cb:() -> Void) {
-		var activePlayerId = background.currentPlayerId;
-		var player = info.players[activePlayerId];
-		var fromGridId = player.atGridId;
-		var moveStep = Math.floor(Math.random() * 6) + 1;
-		var toGridId = fromGridId + moveStep;
+		#if debug
+		// https://haxe.org/manual/lf-condition-compilation.html
+		trace("playerDice");
+		#end
+		final activePlayerId = background.currentPlayerId;
+		final player = info.players[activePlayerId];
+		final fromGridId = player.atGridId;
+		final moveStep = Math.floor(Math.random() * 6) + 1;
+		final toGridId = fromGridId + moveStep;
 		player.atGridId = toGridId;
 		info.actions = [
 			{
@@ -68,7 +72,7 @@ class ModelVer1 extends DebugModel {
 				gameInfo: gameInfo() // Reflect.copy(gameInfo())
 			}
 		];
-		var toGrid = info.grids[toGridId];
+		final toGrid = info.grids[toGridId];
 		info.events = [
 			{
 				id: EventInfoID.WALK_STOP,
@@ -100,8 +104,8 @@ class ModelVer1 extends DebugModel {
 	}
 
 	override function getPreResultOfNego(playerId:Int, gridId:Int, people:People, invite:People):PreResultOnNego {
-		var player = info.players[playerId];
-		var negoCost = getNegoCost(playerId, gridId, people.id, invite.id);
+		final player = info.players[playerId];
+		final negoCost = getNegoCost(playerId, gridId, people.id, invite.id);
 		return {
 			energyAfter: people.energy - Std.int(negoCost.peopleCost.energy),
 			armyBefore: Std.int(player.army),
@@ -115,10 +119,10 @@ class ModelVer1 extends DebugModel {
 	}
 
 	override function takeNegoOn(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int, cb:(gameInfo:GameInfo) -> Void) {
-		var p1 = getPeopleById(p1SelectId);
-		var p2 = getPeopleById(p2SelectId);
-		var player = info.players[playerId];
-		var evt = {
+		final p1 = getPeopleById(p1SelectId);
+		final p2 = getPeopleById(p2SelectId);
+		final player = info.players[playerId];
+		final evt = {
 			id: EventInfoID.NEGOTIATE_RESULT,
 			value: {
 				success: false,
@@ -133,7 +137,7 @@ class ModelVer1 extends DebugModel {
 				foodAfter: player.food,
 			}
 		};
-		var success = applyNegoCost(playerId, gridId, p1SelectId, p2SelectId);
+		final success = applyNegoCost(playerId, gridId, p1SelectId, p2SelectId);
 		evt.value.success = success;
 		evt.value.energyAfter = p1.energy;
 		evt.value.armyAfter = player.army;
@@ -146,20 +150,20 @@ class ModelVer1 extends DebugModel {
 
 	// 交涉(智力/政治/魅力)
 	function getNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
-		var grid = info.grids[gridId];
-		var fightPeople = [p1SelectId, p2SelectId].map(getPeopleById);
+		final grid = info.grids[gridId];
+		final fightPeople = [p1SelectId, p2SelectId].map(getPeopleById);
 		switch fightPeople {
 			case [p1, p2]:
 				// 用掉1/5的體力(最多20)
 				// 體力越少效率越低
-				var useEnergy = p1.energy / 5;
+				final useEnergy = p1.energy / 5;
 				// 使用20體力的情況下基礎值為0.5
-				var base = (useEnergy / 100) + 0.3;
-				var intelligenceFactor = p1.intelligence / p2.intelligence;
-				var politicalFactor = p1.political / p2.political;
-				var charmFactor = p1.charm / p2.charm;
-				var rate = base * intelligenceFactor * politicalFactor * charmFactor;
-				var gainRate = 0.1 * rate + 0.1;
+				final base = (useEnergy / 100) + 0.3;
+				final intelligenceFactor = p1.intelligence / p2.intelligence;
+				final politicalFactor = p1.political / p2.political;
+				final charmFactor = p1.charm / p2.charm;
+				final rate = base * intelligenceFactor * politicalFactor * charmFactor;
+				final gainRate = 0.1 * rate + 0.1;
 				return {
 					playerCost: {
 						id: playerId,
@@ -173,15 +177,15 @@ class ModelVer1 extends DebugModel {
 					},
 					successRate: rate
 				};
-			default:
+			case _:
 				throw new haxe.Exception("fightPeople not right");
 		}
 	}
 
 	function applyNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int):Bool {
-		var negoCost = getNegoCost(playerId, gridId, p1SelectId, p2SelectId);
+		final negoCost = getNegoCost(playerId, gridId, p1SelectId, p2SelectId);
 		// 無論成功或失敗武將先消體力
-		var people = getPeopleById(p1SelectId);
+		final people = getPeopleById(p1SelectId);
 		people.energy -= Std.int(negoCost.peopleCost.energy);
 		//
 		var success = Math.random() < negoCost.successRate;
@@ -189,12 +193,12 @@ class ModelVer1 extends DebugModel {
 			return false;
 		}
 		// 城池被搶奪
-		var grid = info.grids[gridId];
+		final grid = info.grids[gridId];
 		grid.army -= negoCost.playerCost.army;
 		grid.money -= negoCost.playerCost.money;
 		grid.food -= negoCost.playerCost.food;
 		// 玩家搶奪
-		var player = info.players[playerId];
+		final player = info.players[playerId];
 		player.army += negoCost.playerCost.army;
 		player.money += negoCost.playerCost.money;
 		player.food += negoCost.playerCost.food;
@@ -214,8 +218,8 @@ class ModelVer1 extends DebugModel {
 	}
 
 	override function getPreResultOfExplore(playerId:Int, gridId:Int, people:People, invite:People):PreResultOnExplore {
-		var player = info.players[playerId];
-		var cost = getExploreCost(playerId, gridId, people.id, invite.id);
+		final player = info.players[playerId];
+		final cost = getExploreCost(playerId, gridId, people.id, invite.id);
 		return {
 			energyAfter: people.energy - Std.int(cost.peopleCost.energy),
 			successRate: cost.successRate
@@ -223,9 +227,9 @@ class ModelVer1 extends DebugModel {
 	}
 
 	override function takeExplore(playerId:Int, gridId:Int, p1SelectId:Int, exploreId:Int, cb:(gameInfo:GameInfo) -> Void) {
-		var p1 = getPeopleById(p1SelectId);
-		var p2 = getPeopleById(exploreId);
-		var preResult = getPreResultOfExplore(playerId, gridId, p1, p2);
+		final p1 = getPeopleById(p1SelectId);
+		final p2 = getPeopleById(exploreId);
+		final preResult = getPreResultOfExplore(playerId, gridId, p1, p2);
 		info.events = [
 			{
 				id: EventInfoID.EXPLORE_RESULT,
@@ -250,11 +254,11 @@ class ModelVer1 extends DebugModel {
 	// 佔領
 	// =================================
 	override function getTakeWarPreview(playerId:Int, gridId:Int):WarPreview {
-		var grid = info.grids[gridId];
+		final grid = info.grids[gridId];
 		if (grid.buildtype == BUILDING.EMPTY) {
 			throw new haxe.Exception("空地不能攻擊");
 		}
-		var gridOrPlayer:PlayerInfo = switch info.grids[gridId].belongPlayerId {
+		final gridOrPlayer:PlayerInfo = switch info.grids[gridId].belongPlayerId {
 			case null:
 				{
 					id: grid.id,
@@ -301,7 +305,6 @@ class ModelVer1 extends DebugModel {
 	}
 
 	override function takeWarOn(playerId:Int, gridId:Int, p1PeopleId:Int, p2PeopleId:Int, army1:Float, army2:Float, cb:(gameInfo:GameInfo) -> Void) {
-		var info = gameInfo();
 		info.events = [
 			{
 				id: EventInfoID.WAR_RESULT,
