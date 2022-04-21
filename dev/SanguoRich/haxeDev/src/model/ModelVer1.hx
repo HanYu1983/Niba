16b9eebce3ff29ea75eafc41f0d164163f3f3179
HanYu1983 <hanyushome@gmail.com>
2022-04-21 18:37:47 +0800

    add nego function

--- dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
+++ dev/SanguoRich/haxeDev/src/model/ModelVer1.hx
@@ -11,6 +11,7 @@ import model.IModel.ActionInfoID;
 import model.IModel.EventInfoID;
 import model.IModel.PlayerInfo;
 import model.IModel.GameInfo;
+import model.IModel.EventInfo;
 import model.GridGenerator.Grid;
 import model.PeopleGenerator.People;
 
@@ -87,6 +88,10 @@ class ModelVer1 extends DebugModel {
 		cb();
 	}
 
+	// =================================
+	// 交涉
+	// 向城池奪取%資源
+	// =================================
 	override function getTakeNegoPreview(playerId:Int, gridId:Int):NegoPreview {
 		return {
 			p1ValidPeople: info.players[playerId].people,
@@ -100,11 +105,11 @@ class ModelVer1 extends DebugModel {
 		return {
 			energyAfter: people.energy - negoCost.peopleCost.energy,
 			armyBefore: Std.int(player.army),
-			armyAfter: Std.int(player.army - negoCost.playerCost.army),
+			armyAfter: Std.int(player.army + negoCost.playerCost.army),
 			moneyBefore: Std.int(player.money),
-			moneyAfter: Std.int(player.money - negoCost.playerCost.money),
+			moneyAfter: Std.int(player.money + negoCost.playerCost.money),
 			foodBefore: Std.int(player.food),
-			foodAfter: Std.int(player.food - negoCost.playerCost.food),
+			foodAfter: Std.int(player.food + negoCost.playerCost.food),
 			successRate: negoCost.successRate
 		};
 	}
@@ -112,29 +117,91 @@ class ModelVer1 extends DebugModel {
 	override function takeNegoOn(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int, cb:(gameInfo:GameInfo) -> Void) {
 		var p1 = getPeopleById(p1SelectId);
 		var p2 = getPeopleById(p2SelectId);
-		var preResult = getPreResultOfNego(playerId, gridId, p1, p2);
-		applyNegoCost(playerId, gridId, p1SelectId, p2SelectId);
-		info.events = [
-			{
-				id: EventInfoID.NEGOTIATE_RESULT,
-				value: {
-					success: true,
-					people: p1,
-					energyBefore: p1.energy,
-					energyAfter: preResult.energyAfter,
-					armyBefore: preResult.armyBefore,
-					armyAfter: preResult.armyAfter,
-					moneyBefore: preResult.moneyBefore,
-					moneyAfter: preResult.moneyAfter,
-					foodBefore: preResult.foodBefore,
-					foodAfter: preResult.foodAfter,
-				}
+		var player = info.players[playerId];
+		var evt = {
+			id: EventInfoID.NEGOTIATE_RESULT,
+			value: {
+				success: false,
+				people: p1,
+				energyBefore: p1.energy,
+				energyAfter: p1.energy,
+				armyBefore: player.army,
+				armyAfter: player.army,
+				moneyBefore: player.money,
+				moneyAfter: player.money,
+				foodBefore: player.food,
+				foodAfter: player.food,
 			}
-		];
+		};
+		var success = applyNegoCost(playerId, gridId, p1SelectId, p2SelectId);
+		evt.value.success = success;
+		evt.value.energyAfter = p1.energy;
+		evt.value.armyAfter = player.army;
+		evt.value.moneyAfter = player.money;
+		evt.value.foodAfter = player.food;
+		info.events = [evt];
 		updateGameInfo();
 		cb(info);
 	}
 
+	// 交涉(智力/政治/魅力)
+	function getNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
+		var grid = info.grids[gridId];
+		var fightPeople = [p1SelectId, p2SelectId].map(getPeopleById);
+		switch fightPeople {
+			case [p1, p2]:
+				var base = 0.5;
+				var intelligenceFactor = p1.intelligence / p2.intelligence;
+				var politicalFactor = p1.political / p2.political;
+				var charmFactor = p1.charm / p2.charm;
+				var rate = base * intelligenceFactor * politicalFactor * charmFactor;
+				var gainRate = 0.2 * rate;
+				return {
+					playerCost: {
+						id: playerId,
+						army: grid.army * gainRate,
+						money: grid.money * gainRate,
+						food: grid.food * gainRate
+					},
+					peopleCost: {
+						id: p1.id,
+						energy: 10,
+					},
+					successRate: rate
+				};
+			default:
+				throw new haxe.Exception("fightPeople not right");
+		}
+	}
+
+	function applyNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int):Bool {
+		var negoCost = getNegoCost(playerId, gridId, p1SelectId, p2SelectId);
+		// 無論成功或失敗武將先消體力
+		var people = getPeopleById(p1SelectId);
+		people.energy -= negoCost.peopleCost.energy;
+		//
+		var success = Math.random() < negoCost.successRate;
+		if (success == false) {
+			return false;
+		}
+		// 城池被搶奪
+		var grid = info.grids[gridId];
+		grid.army -= negoCost.playerCost.army;
+		grid.money -= negoCost.playerCost.money;
+		grid.food -= negoCost.playerCost.food;
+		// 玩家搶奪
+		var player = info.players[playerId];
+		player.army += negoCost.playerCost.army;
+		player.money += negoCost.playerCost.money;
+		player.food += negoCost.playerCost.food;
+		updateGameInfo();
+		return true;
+	}
+
+	// =================================
+	// 探索
+	// 找武將
+	// =================================
 	override function getTakeExplorePreview(playerId:Int, gridId:Int):ExplorePreview {
 		return {
 			p1ValidPeople: info.players[playerId].people,
@@ -175,6 +242,9 @@ class ModelVer1 extends DebugModel {
 		cb(info);
 	}
 
+	// =================================
+	// 佔領
+	// =================================
 	override function getTakeWarPreview(playerId:Int, gridId:Int):WarPreview {
 		var grid = info.grids[gridId];
 		if (grid.buildtype == BUILDING.EMPTY) {
@@ -265,33 +335,6 @@ class ModelVer1 extends DebugModel {
 			throw new haxe.Exception('people not found: ${id}');
 		}
 		return find[0];
-	}
-
-	function getNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
-		var fightPeople = [p1SelectId, p2SelectId].map(getPeopleById);
-		switch fightPeople {
-			case [p1, p2]:
-				return {
-					playerCost: {
-						id: playerId,
-						army: 10,
-						money: 10,
-						food: 10
-					},
-					peopleCost: {
-						id: p1.id,
-						energy: 10,
-					},
-					successRate: 0.7
-				};
-			default:
-				throw new haxe.Exception("fightPeople not right");
-		}
-	}
-
-	function applyNegoCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
-		var negoCost = getNegoCost(playerId, gridId, p1SelectId, p2SelectId);
-		// TODO
 	}
 
 	function getExploreCost(playerId:Int, gridId:Int, p1SelectId:Int, p2SelectId:Int) {
