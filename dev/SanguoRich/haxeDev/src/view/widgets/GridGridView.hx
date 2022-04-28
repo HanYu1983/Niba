5c07bd51c291feb0ac9d62964aa5c8773edfcc08
vicyu <finalsteve@hotmail.com>
2022-04-28 19:34:49 +0800

    優化介面

--- dev/SanguoRich/haxeDev/src/view/widgets/GridGridView.hx
+++ dev/SanguoRich/haxeDev/src/view/widgets/GridGridView.hx
@@ -1,5 +1,6 @@
 package view.widgets;
 
+import model.GridGenerator.BUILDING;
 import model.GridGenerator.Grid;
 import model.IModel.PlayerInfo;
 import haxe.ui.containers.properties.Property;
@@ -25,13 +26,17 @@ class GridGridView extends BasicGridView{
         pro_money.value = '${Math.floor(grid.money)} (成:${Main.getRateString(grid.moneyGrow)})';
         pro_food.value = '${Math.floor(grid.food)} (成:${Main.getRateString(grid.foodGrow)})';
         pro_army.value = '${Math.floor(grid.army)} (成:${Main.getRateString(grid.armyGrow)})';
-        pro_favor.value = Main.getFavorString(grid.favor[gameInfo.currentPlayer.id]);
         pro_peopleCount.value = grid.people.length;
         
         if(grid.belongPlayerId != null){
             pro_player.value = gameInfo.players[grid.belongPlayerId].name;
         }else{
             pro_player.value = "無";
+        }
+        if(grid.buildtype != BUILDING.EMPTY){
+            pro_favor.value = Main.getFavorString(grid.favor[gameInfo.currentPlayer.id]);
+        }else{
+            pro_favor.value = "無";
         }
     }
 }
