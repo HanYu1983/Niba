a8fa1baad21ce07a523dddaac022df5a6b518763
HanYu1983 <hanyushome@gmail.com>
2022-05-03 16:58:51 +0800

    緩兵之計

--- dev/SanguoRich/haxeDev/src/model/ver2/alg/Alg.hx
+++ dev/SanguoRich/haxeDev/src/model/ver2/alg/Alg.hx
@@ -205,20 +205,20 @@ function doPlayerDice(ctx:Context) {
 	final activePlayerId = ctx.currentPlayerId;
 	final player = ctx.players[activePlayerId];
 	final fromGridId = player.position;
-	var moveStep = Math.floor(Math.random() * 6) + 1;
+	final moveStep = Math.floor(Math.random() * 6) + 1;
+	var toGridId = (fromGridId + moveStep) % ctx.grids.length;
 	{
 		// 計算路障
-		final everyStep = [for (i in 1...moveStep) player.position + i].map(s -> s % ctx.grids.length);
+		final everyStep = [for (i in 1...(moveStep + 1)) player.position + i].map(s -> s % ctx.grids.length);
 		final findGroundItem = ctx.groundItems.filter(item -> everyStep.has(item.position) && item.belongToPlayerId != activePlayerId);
 		if (findGroundItem.length > 0) {
 			final stopItem = findGroundItem[0];
 			// 停住
-			moveStep = stopItem.position - player.position;
+			toGridId = stopItem.position;
 			// 移除路障
 			ctx.groundItems = ctx.groundItems.filter(item -> item.id != stopItem.id);
 		}
 	}
-	final toGridId = (fromGridId + moveStep) % ctx.grids.length;
 	player.position = toGridId;
 	player.memory.hasDice = true;
 	ctx.actions.push(Action.MOVE({
