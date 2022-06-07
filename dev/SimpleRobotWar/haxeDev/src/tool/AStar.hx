a64f3943d50c39d8aac1a5fdd567890e337921f8
HanYu1983 <hanyushome@gmail.com>
2022-06-07 20:14:40 +0800

    no message

--- dev/SimpleRobotWar/haxeDev/src/tool/AStar.hx
+++ dev/SimpleRobotWar/haxeDev/src/tool/AStar.hx
@@ -5,56 +5,29 @@ import haxe.ds.List;
 import haxe.ds.ListSort;
 import haxe.ds.StringMap;
 import haxe.ds.BalancedTree;
+import haxe.ds.ObjectMap;
 
 using StringTools;
 
 interface ISolution {
-	function getId():String;
+	function getId():Dynamic;
 	function getSortKey():String;
 	function isGoal():Bool;
 }
 
-function getAStar(getNextSolution:ISolution->Array<ISolution>, isContinueWhenFind:Bool, s:ISolution):StringMap<ISolution> {
-	final close = new StringMap<ISolution>();
-	// final open = new Array<ISolution>();
+function getAStar(getNextSolution:ISolution->Array<ISolution>, isContinueWhenFind:Bool, s:ISolution):ObjectMap<Dynamic, ISolution> {
+	final close = new ObjectMap<Dynamic, ISolution>();
 	final open = new BalancedTree<String, ISolution>();
-	final openMapping = new StringMap<ISolution>();
+	final openMapping = new ObjectMap<Dynamic, ISolution>();
 	open.set(s.getSortKey(), s);
-	// open.push(s);
 	openMapping.set(s.getId(), s);
 	var isDone = false;
-	for (i in 0...1000) {
+	for (i in 0...100) {
+		trace(i);
 		if (isDone) {
 			break;
 		}
-		// open.sort((a, b) -> {
-		// 	return a.compare(b);
-		// });
-		// trace(i, open);
-		// final top = open.pop();
-		// if (top == null) {
-		// 	break;
-		// }
-		// final nextNodes = getNextSolution(top);
-		// for (nextNode in nextNodes) {
-		// 	if (close.exists(nextNode.getId())) {
-		// 		continue;
-		// 	}
-		// 	final origin = openMapping.get(nextNode.getId());
-		// 	if (origin == null) {
-		// 		open.push(nextNode);
-		// 		openMapping.set(nextNode.getId(), nextNode);
-		// 	} else {
-		// 		if (nextNode.compare(origin) > 0) {
-		// 			open.remove(origin);
-		// 			open.push(nextNode);
-		// 			openMapping.set(nextNode.getId(), nextNode);
-		// 		}
-		// 	}
-		// }
-		// open.remove(top);
-		// openMapping.remove(top.getId());
-		// close.set(top.getId(), top);
+		var hasProcess = false;
 		for (top in open.iterator()) {
 			open.remove(top.getSortKey());
 			openMapping.remove(top.getId());
@@ -70,11 +43,6 @@ function getAStar(getNextSolution:ISolution->Array<ISolution>, isContinueWhenFin
 				if (close.exists(nextNode.getId())) {
 					continue;
 				}
-				if (isContinueWhenFind == false) {
-					if (nextNode.isGoal()) {
-						break;
-					}
-				}
 				final origin = openMapping.get(nextNode.getId());
 				if (origin == null) {
 					open.set(nextNode.getSortKey(), nextNode);
@@ -87,6 +55,11 @@ function getAStar(getNextSolution:ISolution->Array<ISolution>, isContinueWhenFin
 					}
 				}
 			}
+			hasProcess = true;
+			break;
+		}
+		// 樹為空就退出
+		if (hasProcess == false) {
 			break;
 		}
 	}
@@ -94,26 +67,24 @@ function getAStar(getNextSolution:ISolution->Array<ISolution>, isContinueWhenFin
 }
 
 class AStarSolution implements ISolution {
-	public final id:String;
+	public final id:Dynamic;
 	public final parentId:Null<String>;
 	public final cost:Int;
 	public final estimate:Int;
 	public final _isGoal:Bool;
-	public final payload:Dynamic;
 
 	final key:String;
 
-	public function new(id:String, parentId:Null<String>, cost:Int, estimate:Int, isGoal:Bool, payload:Dynamic) {
+	public function new(id:Dynamic, parentId:Null<String>, cost:Int, estimate:Int, isGoal:Bool) {
 		this.id = id;
 		this.parentId = parentId;
 		this.cost = cost;
 		this.estimate = estimate;
 		this._isGoal = isGoal;
-		this.payload = payload;
 		key = Std.string(cost + estimate).lpad("0", 5) + "_" + id;
 	}
 
-	public function getId():String {
+	public function getId():Dynamic {
 		return id;
 	}
 
@@ -127,13 +98,13 @@ class AStarSolution implements ISolution {
 }
 
 function test() {
-	final firstNode = new AStarSolution("0_0", null, 0, 9999999, false, [0, 0]);
+	final firstNode = new AStarSolution([0, 0], null, 0, 9999999, false);
 	final tree = getAStar(node -> {
 		final tmp = cast(node, AStarSolution);
-		if (tmp.cost >= 2) {
+		if (tmp.cost >= 100) {
 			return [];
 		}
-		return switch tmp.payload {
+		return switch tmp.id {
 			case [x, y]:
 				[
 					for (next in [[0, -1], [0, 1], [-1, 0], [1, 0]]) {
@@ -142,11 +113,12 @@ function test() {
 								final nx = x + ox;
 								final ny = y + oy;
 								final cost = tmp.cost + 1;
-								final ex = 1;
-								final ey = 1;
+								final ex = 10;
+								final ey = 10;
 								final estimate = Std.int(Math.pow(ex - nx, 2) + Math.pow(ey - ny, 2));
 								final isGoal = nx == ex && ny == ey;
-								new AStarSolution('${nx}_${ny}', tmp.getId(), cost, estimate, isGoal, [nx, ny]);
+								// trace(nx, ny, isGoal);
+								new AStarSolution([nx, ny], tmp.getId(), cost, estimate, isGoal);
 							case _:
 								throw new Exception("next not found");
 						}
@@ -157,4 +129,15 @@ function test() {
 		}
 	}, false, firstNode);
 	trace(tree);
+	for (pos in tree.keys()) {
+		// trace(pos);
+		switch cast(pos, Array<Dynamic>) {
+			case [2, 2]:
+				trace("OK");
+			case [x, y]:
+				trace(x, y);
+			case _:
+				trace("-");
+		}
+	}
 }
