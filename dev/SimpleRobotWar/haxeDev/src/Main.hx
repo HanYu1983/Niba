08ebf3c37a0b2e0148fb5894ac4dd548e263b553
HanYu1983 <hanyushome@gmail.com>
2022-06-07 19:07:04 +0800

    no message

--- dev/SimpleRobotWar/haxeDev/src/Main.hx
+++ dev/SimpleRobotWar/haxeDev/src/Main.hx
@@ -8,38 +8,29 @@ import haxe.Unserializer;
 
 class Main {
 	public static function main() {
-		final model:IModel = new Model();
-		{
-			final robot1 = model.createRobot();
-			model.push(robot1);
-			final robot2 = model.createRobot();
-			model.push(robot2);
-			final pilot1 = model.createPilot();
-			model.push(pilot1);
-			model.setPilotRobot(pilot1, robot1);
-		}
-		Serializer.USE_CACHE = true;
-		final s = Serializer.run(model);
-		trace(s);
-		final model2 = Unserializer.run(s);
-		trace(model2);
-		{
-			final robot1 = model.getRobots()[0];
-			final robot2 = model.getRobots()[1];
-			final pilot1 = model.getPilots()[0];
-			trace(robot1.getPilot() == robot2.getPilot());
-			trace(robot1.getPilot() == pilot1);
-		}
+		tool.AStar.test();
+
+		// final model:IModel = new Model();
 		// {
 		// 	final robot1 = model.createRobot();
-		// 	model.addObject(robot1);
+		// 	model.push(robot1);
 		// 	final robot2 = model.createRobot();
-		// 	model.addObject(robot2);
+		// 	model.push(robot2);
 		// 	final pilot1 = model.createPilot();
-		// 	model.addObject(pilot1);
-		// 	model2.setPilot(robot1, pilot1);
-		// 	model2.setPilot(robot2, pilot1);
+		// 	model.push(pilot1);
+		// 	model.setPilotRobot(pilot1, robot1);
 		// }
+		// Serializer.USE_CACHE = true;
+		// final s = Serializer.run(model);
+		// trace(s);
+		// final model2 = Unserializer.run(s);
 		// trace(model2);
+		// {
+		// 	final robot1 = model.getRobots()[0];
+		// 	final robot2 = model.getRobots()[1];
+		// 	final pilot1 = model.getPilots()[0];
+		// 	trace(robot1.getPilot() == robot2.getPilot());
+		// 	trace(robot1.getPilot() == pilot1);
+		// }
 	}
 }
--- dev/SimpleRobotWar/haxeDev/src/tool/AStar.hx
+++ dev/SimpleRobotWar/haxeDev/src/tool/AStar.hx
@@ -1,78 +1,160 @@
 package tool;
 
+import haxe.Exception;
 import haxe.ds.List;
 import haxe.ds.ListSort;
-import haxe.ds.ObjectMap;
+import haxe.ds.StringMap;
 import haxe.ds.BalancedTree;
 
 using StringTools;
 
-class Solution {
-	public final id:String;
-	public final parentId:String;
-	public final cost:Int;
-	public final estimate:Int;
-	public final payload:Any;
-
-	final key:String;
-
-	public function new(id:String, parentId:String, cost:Int, estimate:Int, payload:Any) {
-		this.id = id;
-		this.parentId = parentId;
-		this.cost = cost;
-		this.estimate = estimate;
-		this.payload = payload;
-		key = Std.string(getScore()).lpad("0", 5) + "_" + id;
-	}
-
-	public function getScore():Int {
-		return cost + estimate;
-	}
-
-	public function compare(other:Solution):Int {
-		// 只有自己和自己比才會回傳0
-		// 回傳0才能表找到自己
-		// https://github.com/HaxeFoundation/haxe/blob/4.2.1/std/haxe/ds/BalancedTree.hx
-		// https://shubo.io/iterative-binary-tree-traversal/
-		return Reflect.compare(this.key, other.key);
-	}
+interface ISolution {
+	function getId():String;
+	function getSortKey():String;
+	function isGoal():Bool;
 }
 
-function getAStar(getNextNode:Solution->Array<Solution>, s:Solution):ObjectMap<String, Solution> {
-	final close = new ObjectMap<String, Solution>();
-	final open = new BalancedTree<Solution, Solution>();
-	final openMapping = new ObjectMap<String, Solution>();
-	open.set(s, s);
-	openMapping.set(s.id, s);
+function getAStar(getNextSolution:ISolution->Array<ISolution>, isContinueWhenFind:Bool, s:ISolution):StringMap<ISolution> {
+	final close = new StringMap<ISolution>();
+	// final open = new Array<ISolution>();
+	final open = new BalancedTree<String, ISolution>();
+	final openMapping = new StringMap<ISolution>();
+	open.set(s.getSortKey(), s);
+	// open.push(s);
+	openMapping.set(s.getId(), s);
+	var isDone = false;
 	for (i in 0...1000) {
-		var hasProcess = false;
+		if (isDone) {
+			break;
+		}
+		// open.sort((a, b) -> {
+		// 	return a.compare(b);
+		// });
+		// trace(i, open);
+		// final top = open.pop();
+		// if (top == null) {
+		// 	break;
+		// }
+		// final nextNodes = getNextSolution(top);
+		// for (nextNode in nextNodes) {
+		// 	if (close.exists(nextNode.getId())) {
+		// 		continue;
+		// 	}
+		// 	final origin = openMapping.get(nextNode.getId());
+		// 	if (origin == null) {
+		// 		open.push(nextNode);
+		// 		openMapping.set(nextNode.getId(), nextNode);
+		// 	} else {
+		// 		if (nextNode.compare(origin) > 0) {
+		// 			open.remove(origin);
+		// 			open.push(nextNode);
+		// 			openMapping.set(nextNode.getId(), nextNode);
+		// 		}
+		// 	}
+		// }
+		// open.remove(top);
+		// openMapping.remove(top.getId());
+		// close.set(top.getId(), top);
 		for (top in open.iterator()) {
-			final nextNodes = getNextNode(top);
+			open.remove(top.getSortKey());
+			openMapping.remove(top.getId());
+			close.set(top.getId(), top);
+			if (isContinueWhenFind == false) {
+				if (top.isGoal()) {
+					isDone = true;
+					break;
+				}
+			}
+			final nextNodes = getNextSolution(top);
 			for (nextNode in nextNodes) {
-				if (close.exists(nextNode.id)) {
+				if (close.exists(nextNode.getId())) {
 					continue;
 				}
-				final origin = openMapping.get(nextNode.id);
+				if (isContinueWhenFind == false) {
+					if (nextNode.isGoal()) {
+						break;
+					}
+				}
+				final origin = openMapping.get(nextNode.getId());
 				if (origin == null) {
-					open.set(nextNode, nextNode);
-					openMapping.set(nextNode.id, nextNode);
+					open.set(nextNode.getSortKey(), nextNode);
+					openMapping.set(nextNode.getId(), nextNode);
 				} else {
-					if (nextNode.getScore() < origin.getScore()) {
-						open.remove(origin);
-						open.set(nextNode, nextNode);
-						openMapping.set(nextNode.id, nextNode);
+					if (nextNode.getSortKey() < origin.getSortKey()) {
+						open.remove(origin.getSortKey());
+						open.set(nextNode.getSortKey(), nextNode);
+						openMapping.set(nextNode.getId(), nextNode);
 					}
 				}
 			}
-			open.remove(top);
-			openMapping.remove(top.id);
-			close.set(top.id, top);
-			hasProcess = true;
-			break;
-		}
-		if (hasProcess == false) {
 			break;
 		}
 	}
 	return close;
+}
+
+class AStarSolution implements ISolution {
+	public final id:String;
+	public final parentId:Null<String>;
+	public final cost:Int;
+	public final estimate:Int;
+	public final _isGoal:Bool;
+	public final payload:Dynamic;
+
+	final key:String;
+
+	public function new(id:String, parentId:Null<String>, cost:Int, estimate:Int, isGoal:Bool, payload:Dynamic) {
+		this.id = id;
+		this.parentId = parentId;
+		this.cost = cost;
+		this.estimate = estimate;
+		this._isGoal = isGoal;
+		this.payload = payload;
+		key = Std.string(cost + estimate).lpad("0", 5) + "_" + id;
+	}
+
+	public function getId():String {
+		return id;
+	}
+
+	public function getSortKey():String {
+		return key;
+	}
+
+	public function isGoal():Bool {
+		return _isGoal;
+	}
+}
+
+function test() {
+	final firstNode = new AStarSolution("0_0", null, 0, 9999999, false, [0, 0]);
+	final tree = getAStar(node -> {
+		final tmp = cast(node, AStarSolution);
+		if (tmp.cost >= 2) {
+			return [];
+		}
+		return switch tmp.payload {
+			case [x, y]:
+				[
+					for (next in [[0, -1], [0, 1], [-1, 0], [1, 0]]) {
+						switch next {
+							case [ox, oy]:
+								final nx = x + ox;
+								final ny = y + oy;
+								final cost = tmp.cost + 1;
+								final ex = 1;
+								final ey = 1;
+								final estimate = Std.int(Math.pow(ex - nx, 2) + Math.pow(ey - ny, 2));
+								final isGoal = nx == ex && ny == ey;
+								new AStarSolution('${nx}_${ny}', tmp.getId(), cost, estimate, isGoal, [nx, ny]);
+							case _:
+								throw new Exception("next not found");
+						}
+					}
+				];
+			case _:
+				throw new Exception("payload not found");
+		}
+	}, false, firstNode);
+	trace(tree);
 }
