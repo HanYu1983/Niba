d6b8d69749d8928ea0aa8ec8542a1c120bfe702e
余漢平 <yuhanping@wangyuan.com>
2022-06-09 12:35:27 +0800

    no message

---
+++ dev/SimpleRobotWar/haxeDev/src/vic/widgets/HaxeUIView.hx
@@ -0,0 +1,10 @@
+package vic.widgets;
+
+import haxe.ui.containers.Box;
+
+@:build(haxe.ui.ComponentBuilder.build('vic/widgets/HaxeUIView.xml'))
+class HaxeUIView extends Box {
+	public function new() {
+		super();
+	}
+}
--- dev/SimpleRobotWar/haxeDev/src/vic/widgets/LobbyPage.hx
+++ dev/SimpleRobotWar/haxeDev/src/vic/widgets/LobbyPage.hx
@@ -1,11 +1,16 @@
 package vic.widgets;
 
+import haxe.ui.events.MouseEvent;
 import haxe.ui.containers.Box;
 
 @:build(haxe.ui.ComponentBuilder.build('vic/widgets/LobbyPage.xml'))
-class LobbyPage extends Box{
-    public function new() {
-        super();
-    }
+class LobbyPage extends Box {
+	public function new() {
+		super();
+	}
 
+    @:bind(btn_game, MouseEvent.CLICK)
+    function onBtnGameClick(e){
+        Main.defaultView.getLobbyController().onEvent(ON_CLICK_GOTO_BATTLE("參數之後再想"));
+    }
 }
