f22a121b2f61cbd7c04febce2361aa052245422c
余漢平 <yuhanping@wangyuan.com>
2022-06-10 15:04:53 +0800

    change to dark theme

--- dev/SimpleRobotWar/haxeDev/src/Main.hx
+++ dev/SimpleRobotWar/haxeDev/src/Main.hx
@@ -1,5 +1,7 @@
 package;
 
+import haxe.ui.themes.Theme;
+import haxe.ui.themes.ThemeManager;
 import haxe.Exception;
 import haxe.ui.core.Component;
 import haxe.ui.HaxeUIApp;
@@ -15,6 +17,8 @@ class Main {
 			view = new HaxeUIView();
 			app.addComponent(view);
 			final ctr = new Controller(view);
+
+			ThemeManager.instance.applyTheme(Theme.DARK);
 		});
 	}
 }
