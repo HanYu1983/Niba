28c83646a62b1a34ea7a09c041ed62c4c2ade5dd
HanYu1983 <hanyushome@gmail.com>
2022-06-10 19:54:00 +0800

    no message

--- dev/SimpleRobotWar/haxeDev/src/han/alg/Path.hx
+++ dev/SimpleRobotWar/haxeDev/src/han/alg/Path.hx
@@ -1,5 +1,6 @@
 package han.alg;
 
+import haxe.macro.Context;
 import haxe.Serializer;
 import haxe.Unserializer;
 import haxe.Exception;
@@ -9,11 +10,21 @@ import han.model.IDefine;
 import tool.optalg.Define;
 import tool.optalg.AStar;
 
+function getGridMoveFactor(ctx:Context, id:Position):Array<Float> {
+	final grid = ctx.grids.get(id);
+	if (grid == null) {
+		throw new Exception('grid not found:${id}');
+	}
+	final terrian = TERRIANS[grid.terrianId];
+	return terrian.moveFactor;
+}
+
 private class PathSolution extends DefaultSolution<Position> {
 	final _ctx:Context;
 	final _targetPos:Position;
 
-	public function new(id:Position, parentId:Null<Position>, cost:Int, estimate:Int, isGoal:Bool, ctx:Context, targetPos:Position) {
+	public function new(ctx:Context, id:Position, targetPos:Position, parentId:Null<Position> = null, cost:Int = 0, estimate:Int = 99999,
+			isGoal:Bool = false) {
 		super(id, parentId, cost, estimate, isGoal);
 		_ctx = ctx;
 		_targetPos = targetPos;
@@ -22,24 +33,79 @@ private class PathSolution extends DefaultSolution<Position> {
 	override function getNextSolution():Array<ISolution<Position>> {
 		return switch [getId(), _targetPos] {
 			case [Position.POS(x, y), Position.POS(ex, ey)]:
-				final fixDot = 10.0;
 				[
 					for (next in [[0, -1], [0, 1], [-1, 0], [1, 0]]) {
-						switch next {
-							case [ox, oy]:
-								final nx = x + ox;
-								final ny = y + oy;
-								final remainX:Int = Std.int(Math.abs(ex - nx));
-								final remainY:Int = Std.int(Math.abs(ey - ny));
-								final isGoal = nx == ex && ny == ey;
-								final cost = this.cost + 1;
-								final estimate = (remainX + remainY) << 1;
-								new PathSolution(POS(nx, ny), getId(), cost, estimate, isGoal, _ctx, _targetPos);
+						final nx = switch next {
+							case [ox, _]:
+								x + ox;
 							case _:
 								throw new Exception("next not found");
+						};
+						final ny = switch next {
+							case [_, oy]:
+								y + oy;
+							case _:
+								throw new Exception("next not found");
+						};
+						if (nx >= 0 && ny >= 0 && nx < 10 && ny < 10) {
+							final remainX:Int = Std.int(Math.abs(ex - nx));
+							final remainY:Int = Std.int(Math.abs(ey - ny));
+							final estimate = (remainX + remainY) << 3;
+							final isGoal = nx == ex && ny == ey;
+							final moveCost = switch getGridMoveFactor(_ctx, POS(nx, ny)) {
+								case [sea, ground, forest, mountain]:
+									Std.int(4 * sea);
+								case _:
+									throw new Exception("getGridMoveFactor not found");
+							}
+							final cost = this.cost + moveCost;
+							new PathSolution(_ctx, POS(nx, ny), _targetPos, getId(), cost, estimate, isGoal);
 						}
 					}
 				];
+			case _:
+				throw new Exception("payload not found");
+		}
+	}
+}
+
+private class ShortestTreeSolution extends DefaultSolution<Position> {
+	final _ctx:Context;
+	final _energy:Int;
+
+	public function new(ctx:Context, id:Position, energy:Int, parentId:Null<Position> = null, cost:Int = 0, estimate:Int = 0, isGoal:Bool = false) {
+		super(id, parentId, cost, estimate, isGoal);
+		_ctx = ctx;
+		_energy = energy;
+	}
+
+	override function getNextSolution():Array<ISolution<Position>> {
+		return switch getId() {
+			case Position.POS(x, y):
+				final ret:Array<ISolution<Position>> = [];
+				for (next in [[0, -1], [0, 1], [-1, 0], [1, 0]]) {
+					switch next {
+						case [ox, oy]:
+							final nx = x + ox;
+							final ny = y + oy;
+							if (nx >= 0 && ny >= 0 && nx < 10 && ny < 10) {
+								final moveCost = switch getGridMoveFactor(_ctx, POS(nx, ny)) {
+									case [sea, ground, forest, mountain]:
+										Std.int(4 * sea);
+									case _:
+										throw new Exception("getGridMoveFactor not found");
+								}
+								final cost = this.cost + moveCost;
+								if (cost < _energy) {
+									final nextSolution = new ShortestTreeSolution(_ctx, POS(nx, ny), _energy, getId(), cost, 0, false);
+									ret.push(nextSolution);
+								}
+							}
+						case _:
+							throw new Exception("next not found");
+					}
+				}
+				ret;
 			case _:
 				throw new Exception("payload not found");
 		}
@@ -53,10 +119,20 @@ function test() {
 private function testPath() {
 	final _ctx = getDefaultContext();
 	_ctx.grids = getRandomMap(10, 10);
+	final robot = createRobot('${_ctx.idSeq++}');
+	_ctx.robots.set(robot.id, robot);
 	final s = Serializer.run(_ctx);
 	final ctx = Unserializer.run(s);
-	final firstSolution = new PathSolution(POS(0, 0), null, 0, 9999999, false, ctx, POS(10, 10));
-	final tree = getAStar(firstSolution, {exitWhenFind: true});
-	final path = getPath(tree, POS(10, 10));
-	trace(path.length);
+	{
+		final tree = getAStar(new PathSolution(ctx, POS(0, 0), POS(9, 9)), {exitWhenFind: true});
+		// trace(tree);
+		final path = getPath(tree, POS(9, 9));
+		trace(path);
+	}
+	{
+		final tree = getAStar(new ShortestTreeSolution(ctx, POS(4, 4), 20), {exitWhenFind: true});
+		for (pos => solution in tree) {
+			trace(pos, solution.getSortScore());
+		}
+	}
 }
