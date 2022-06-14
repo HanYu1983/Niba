ff0c84efd7d1b730f6761fc53ae890d524763c2b
余漢平 <yuhanping@wangyuan.com>
2022-06-14 16:18:14 +0800

    加入顯示是否行動結束

--- dev/SimpleRobotWar/haxeDev/src/vic/widgets/Robot.hx
+++ dev/SimpleRobotWar/haxeDev/src/vic/widgets/Robot.hx
@@ -4,23 +4,30 @@ import common.IDefine.Position;
 import haxe.ui.containers.Box;
 
 @:build(haxe.ui.ComponentBuilder.build('vic/widgets/Robot.xml'))
-class Robot extends Box{
+class Robot extends Box {
+	public var pos:Position;
 
-    public var pos:Position;
+	public var title(get, set):String;
 
-    public var title(get, set):String;
-    function set_title(title:String){
-        lbl_title.value = title.substring(0,2);
-        return title;
-    }
+	function set_title(title:String) {
+		lbl_title.value = title.substring(0, 2);
+		return title;
+	}
 
-    function get_title():String {
+	function get_title():String {
 		return lbl_title.value;
 	}
 
-    public function new() {
-        super();
-    }
+	@:isVar
+	public var isDone(null, set):Bool;
 
-	
+	function set_isDone(done:Bool) {
+		isDone = done;
+		isDone ? box_doneCover.show() : box_doneCover.hide();
+		return done;
+	}
+
+	public function new() {
+		super();
+	}
 }
