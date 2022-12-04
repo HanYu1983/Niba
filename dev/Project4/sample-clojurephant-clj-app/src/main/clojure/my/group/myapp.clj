e8f22a21c7353701ea0fe9479127df7ee386fee1
Han Yu <hanyushome@gmail.com>
2022-12-04 14:04:27 +0800

    update

--- dev/Project4/sample-clojurephant-clj-app/src/main/clojure/my/group/myapp.clj
+++ dev/Project4/sample-clojurephant-clj-app/src/main/clojure/my/group/myapp.clj
@@ -1,14 +1,11 @@
 (ns my.group.myapp
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (apply println "Hello," args))
-
 (defn init [ns-name]
   (require (symbol ns-name))
   (let [bar (find-ns (symbol ns-name))]
-    ((ns-resolve bar 'hello-world))))
+    ((ns-resolve bar (symbol "hello-world")))))
 
-(init "tool.lib1")
+(defn -main
+  [& args]
+  (init "tool.lib1"))
