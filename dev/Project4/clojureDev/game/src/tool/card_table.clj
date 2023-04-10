b1ece63bb899ba5aa69ac611bdc367d831b4b7c6
DESKTOP-4C0HLGI\johny <hanyu>
2023-04-11 00:16:37 +0800

    no message

--- dev/Project4/clojureDev/game/src/tool/card_table.clj
+++ dev/Project4/clojureDev/game/src/tool/card_table.clj
@@ -6,5 +6,18 @@
 (s/def ::cards map?)
 (s/def ::table (s/keys :req-un [::card-stacks ::cards]))
 
-(def table {:card-stacks {}
-            :cards {}})
+(def ^:private table {:card-stacks {}
+                      :cards {}})
+
+(defn create-table []
+  table)
+
+(defn add-card [table card-stack-id card])
+
+(defn move-card [table from-card-stack-id to-card-stack-id card-id])
+
+(defn map-card [table f]
+  (map f (:cards table)))
+
+(defn get-card-iterator [table]
+  (:cards table))
