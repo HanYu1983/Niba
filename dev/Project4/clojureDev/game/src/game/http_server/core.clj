ee7aaca983fe9aafa140b831352f8f96f54ff7c2
DESKTOP-4C0HLGI\johny <hanyu>
2023-04-16 19:38:37 +0800

    no message

--- dev/Project4/clojureDev/game/src/game/http_server/core.clj
+++ dev/Project4/clojureDev/game/src/game/http_server/core.clj
@@ -2,16 +2,38 @@
   (:require [clojure.spec.alpha :as s]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer [GET]]
+            [ring.util.response :refer [redirect]]
+            [compojure.core :refer [GET POST]]
             [compojure.route :refer [not-found]]
-            [hiccup.core :refer [html]]))
+            [hiccup.core :refer [html]])
+  (:require [clojure.core.async :refer [go <!! timeout <!]]
+            [clojure.core.match :refer [match]]))
+
+(def model (atom {}))
+
+(defn start-game [model game-id]
+  (go (assoc model :game-id game-id)))
+
+(defn update-model [model evt]
+  (go
+    (match evt
+      [:start-game game-id]
+      (<! (start-game model game-id)))))
+
+(defn page-index []
+  (let [model @model]
+    (html [:div
+           [:div (str "model" model)]
+           [:form {:method "post" :action "fn/start_game"}
+            [:input {:type "text" :name "game-id" :value (:game-id model)}]
+            [:input {:type "submit" :value "start new game"}]]])))
 
 (compojure.core/defroutes app
-  (GET "/" [] (html [:ul
-                     (for [x (range 1 4)]
-                       [:li x])]))
-  (wrap-params (GET "/foobar" {:as r}
-                 (str r)))
+  (GET "/" [] (page-index))
+  (wrap-params (POST "/fn/start_game" [game-id :as r]
+                 (<!! (go (let [next-model (<! (update-model @model [:start-game game-id]))
+                                _ (reset! model next-model)])))
+                 (redirect "/")))
   (wrap-params (GET "/user/:id" [id greeting]
                  (str "<h1>" greeting " user " id "</h1>")))
   (not-found "<h1>Page not found</h1>"))
