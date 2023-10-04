2776802dbd64eee4384d9711fd91350bf20adc02
hanyu <hanyushome@gmail.com>
2023-10-04 16:14:52 +0800

    update

--- dev/Project8/pythonDev2/main.py
+++ dev/Project8/pythonDev2/main.py
@@ -6,7 +6,9 @@ with open('useraction.csv', newline='') as f:
     now = datetime.now()
     def to_insert_sql(row: list[str]) -> str:
         user_id, post_id, rating, comment = row
-        return f"insert into post_comments (user_id, post_id, rating, comment, updated_at, created_at) values ({user_id}, {post_id}, {rating}, \"{comment}\", '{now}', '{now}')"
+        # escape
+        comment = comment.replace("'", "''")
+        return f"insert into post_comments (user_id, post_id, rating, comment, updated_at, created_at) values ({user_id}, {post_id}, {rating}, '{comment}', '{now}', '{now}')"
     sql = ";\n".join([to_insert_sql(row) for row in rows])
     with open('insert_post_comments.sql', 'w', encoding='utf8') as json_file:
         json_file.write(sql)
