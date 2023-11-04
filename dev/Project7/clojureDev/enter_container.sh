eab402a622f16007221671df7aabb2d52107f6b4
hanyu <hanyushome@gmail.com>
2023-11-04 21:27:17 +0800

    no message

--- dev/Project7/clojureDev/enter_container.sh
+++ dev/Project7/clojureDev/enter_container.sh
@@ -1,1 +1,2 @@
+docker container start dev-project7-clojureDev
 docker exec -ti dev-project7-clojureDev bash
---
+++ dev/Project7/clojureDev/game/src/game/define/selection.cljs
@@ -0,0 +1,7 @@
+(ns game.define.selection)
+; ('card "0" "1")
+(defn select-card? [obj] (-> obj first (= 'card)))
+(defn select-basyou? [obj] (-> obj first (= 'basyou)))
+(defn ids [obj] (rest obj))
+(defn select-card [ids] (cons 'card ids))
+(defn select-basyou [ids] (cons 'basyou ids))
