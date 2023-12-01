c401e63fe5fc350657022eacc3e37f4c27a8f9dc
hanyu <hanyushome@gmail.com>
2023-12-01 09:44:54 +0800

    no message

---
+++ dev/Test/bunDev/index.ts
@@ -0,0 +1,8 @@
+// https://bun.sh/docs/quickstart
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        return new Response("Bun!");
+    },
+});
+console.log(`Listening on http://localhost:${server.port} ...`);
