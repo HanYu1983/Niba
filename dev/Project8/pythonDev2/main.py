092147b649f0959500b560064a0d1e62d0caaf6c
hanyu <hanyushome@gmail.com>
2024-01-11 09:31:51 +0800

    update

--- dev/Project8/pythonDev2/main.py
+++ dev/Project8/pythonDev2/main.py
@@ -1,14 +1,23 @@
 import csv
 from datetime import datetime
 
-with open('useraction.csv', newline='') as f:
-    rows = [row for row in csv.reader(f)][1:] # drop header
+filename = "ユーザーアクション - 11_14時点新しく記入する.csv"
+
+with open(filename, newline='') as f:
+    rows = [row for row in csv.reader(f)][3:] # drop header
     now = datetime.now()
     def to_insert_sql(row: list[str]) -> str:
-        user_id, post_id, rating, comment = row
+        # too many values to unpack (expected 8)
+        # post_name, post_url, post_url2, post_id, user_id, manager, rating, comment = row
+        post_id = row[3]
+        user_id = row[4]
+        rating = row[6]
+        comment = row[7]
         # escape
         comment = comment.replace("'", "''")
         return f"insert into post_comments (user_id, post_id, rating, comment, updated_at, created_at) values ({user_id}, {post_id}, {rating}, '{comment}', '{now}', '{now}')"
-    sql = ";\n".join([to_insert_sql(row) for row in rows])
+    rows = [row for row in rows if len(row[0]) > 0]
+    rows = [to_insert_sql(row) for row in rows]
+    sql = ";\n".join(rows)+";"
     with open('insert_post_comments.sql', 'w', encoding='utf8') as json_file:
         json_file.write(sql)
