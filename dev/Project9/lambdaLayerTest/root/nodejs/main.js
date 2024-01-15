96f64681b5a6b85de89d5acc007854de2bd0e50f
hanyu <hanyushome@gmail.com>
2024-01-15 09:33:16 +0800

    update

--- dev/Project9/lambdaLayerTest/root/nodejs/main.js
+++ dev/Project9/lambdaLayerTest/root/nodejs/main.js
@@ -1,16 +1,31 @@
 const axios = require("axios")
-//const lib = require("./lib.js")
+const playwright = require('playwright-aws-lambda');
 
 async function main(event) {
     console.log(process.env.NODE_PATH)
-    // const resp = await axios({url: "http://google.com"})
-    // console.log(resp)
-    // TODO implement
+    let browser = null;
+    try {
+        browser = await playwright.launchChromium();
+        const context = await browser.newContext();
+
+        const page = await context.newPage();
+        await page.goto(event.url || 'https://example.com');
+
+        console.log('Page title: ', await page.title());
+    } catch (error) {
+        throw error;
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
 }
+
+
 
 exports.handler = main
