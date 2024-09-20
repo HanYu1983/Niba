88712832f8d5e31bb597daba5a892e4764c66349
hanyu <hanyushome@gmail.com>
2024-09-20 21:10:38 +0800

    no message

--- dev/Project9/bunTest/debug/index.ts
+++ dev/Project9/bunTest/debug/index.ts
@@ -1,4 +1,4 @@
-import { testGetPlayEffects } from "../game/gameState/getPlayEffects";
+import { getPlayEffects, testGetPlayEffects } from "../game/gameState/getPlayEffects";
 import { testBattleBonus, testFlow1, testFlow2 } from "./testFlow";
 import { tests as itemGroupTests } from "../tool/ItemGroup"
 import { test179028_10D_U_WT181N_white } from "./test179028_10D_U_WT181N_white";
@@ -12,6 +12,14 @@ import { testReturnRuleEffect } from "./testReturnRuleEffect";
 import { testPS } from "./testPS";
 import { testCrossWeapon } from "./testCrossWeapon";
 import { getPrototype, loadPrototype } from "../script";
+import { PlayerA, PlayerB } from "../game/define/PlayerID";
+import { setActivePlayerID } from "../game/gameState/ActivePlayerComponent";
+import { createGameState, GameState } from "../game/gameState/GameState";
+import { setPhase } from "../game/gameState/PhaseComponent";
+import { getPlayCardEffects } from "../game/gameState/getPlayCardEffect";
+import { AbsoluteBaSyouFn } from "../game/define/BaSyou";
+import { createCardWithProtoIds } from "../game/gameState/CardTableComponent";
+import { getGlobalEffects } from "../game/gameState/globalEffects";
 
 export async function tests() {
     return [
@@ -43,8 +51,17 @@ export async function tests() {
 async function testLoadPrototype() {
     const blackT3 = ["179015_04B_O_BK010C_black", "179015_04B_O_BK010C_black", "179015_04B_U_BK058R_black", "179015_04B_U_BK058R_black", "179015_04B_U_BK059C_black", "179015_04B_U_BK059C_black", "179015_04B_U_BK061C_black", "179015_04B_U_BK061C_black", "179016_04B_U_BK066C_black", "179016_04B_U_BK066C_black", "179019_02A_C_BK015S_black", "179019_02A_C_BK015S_black", "179020_05C_U_BK100U_black", "179020_05C_U_BK100U_black", "179023_06C_C_BK048R_black", "179023_06C_C_BK048R_black", "179023_06C_C_BK049U_black", "179023_06C_C_BK049U_black", "179024_04B_C_BK027U_black", "179024_04B_C_BK027U_black", "179024_04B_U_BK060C_black", "179024_04B_U_BK060C_black", "179024_04B_U_BK067C_black", "179024_04B_U_BK067C_black", "179024_B2B_C_BK054C_black", "179024_B2B_C_BK054C_black", "179024_B2B_U_BK128S_black_02", "179024_B2B_U_BK128S_black_02", "179024_B2B_U_BK129R_black", "179024_B2B_U_BK129R_black", "179027_09D_C_BK063R_black", "179027_09D_C_BK063R_black", "179027_09D_O_BK010N_black", "179027_09D_O_BK010N_black", "179027_09D_U_BK163S_black", "179027_09D_U_BK163S_black", "179027_09D_U_BK163S_black", "179029_06C_C_BK045U_black", "179029_06C_C_BK045U_black", "179029_B3C_C_BK071N_black", "179029_B3C_C_BK071N_black", "179029_B3C_U_BK184N_black", "179029_B3C_U_BK184N_black", "179029_B3C_U_BK184N_black", "179029_B3C_U_BK185N_black", "179029_B3C_U_BK185N_black", "179030_11E_U_BK194S_2_black", "179030_11E_U_BK194S_2_black", "179030_11E_U_BK194S_2_black", "179901_B2B_C_BK005P_black"]
     await Promise.all(blackT3.map(loadPrototype))
-    const prototype = getPrototype("179015_04B_O_BK010C_black")
-    console.log(prototype)
-
-
+    let ctx = createGameState()
+    ctx = setActivePlayerID(ctx, PlayerA) as GameState
+    ctx = setPhase(ctx, ["配備フェイズ", "フリータイミング"]) as GameState
+    ctx = setActivePlayerID(ctx, PlayerA) as GameState
+    ctx = createCardWithProtoIds(ctx, AbsoluteBaSyouFn.of(PlayerA, "本国"), blackT3) as GameState
+    ctx = createCardWithProtoIds(ctx, AbsoluteBaSyouFn.of(PlayerB, "本国"), blackT3) as GameState
+    ctx = createCardWithProtoIds(ctx, AbsoluteBaSyouFn.of(PlayerA, "手札"), blackT3.slice(0, 6)) as GameState
+    ctx = createCardWithProtoIds(ctx, AbsoluteBaSyouFn.of(PlayerA, "配備エリア"), blackT3.slice(6, 6)) as GameState
+    for (let i = 0; i < 100; ++i) {
+        getGlobalEffects(ctx, null)
+        const cmd = getPlayEffects(ctx, PlayerA)
+        console.log(`能用的指令數量: ${cmd.length}`)
+    }
 }
