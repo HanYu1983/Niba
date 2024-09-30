482c25663fbcc5d1415a88d211fedfef09953af9
hanyu <hanyushome@gmail.com>
2024-09-30 22:42:54 +0800

    update

--- dev/GundamCard2/bun/src/game/define/GameError.ts
+++ dev/GundamCard2/bun/src/game/define/GameError.ts
@@ -19,7 +19,7 @@ export class GameError extends Error {
 // 所以支付完費用後進入堆疊的效果不能出現TipError，比如執行時要取得對象時(ItemStateFn.getTip)
 // 若出現代表程式流程有誤，
 // 堆疊裡的效果只能經過以下:
-// 若效果引發新的效果時一定會先經過可支付 性的判斷才能加入堆疊(addImmediateEffectIfCanPayCost)，
+// 若效果引發新的效果時一定會先經過可支付性的判斷才能加入堆疊(addImmediateEffectIfCanPayCost)，
 // 出卡或內文等臨時建立的效果也會在flow的流程中經過可支付性的判斷(getPlayerCommandsFilterNoErrorDistinct)，
 // ActionTitleFn中就丟TargetMissingError，非預期錯誤則丟Error
 export class TargetMissingError extends GameError {
