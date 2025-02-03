3b738c837ca297f46cd872c99b0f1ac551f6739a
hanyu <hanyushome@gmail.com>
2025-02-03 17:10:29 +0800

    update

--- dev/Project13/html/js/app.view.js
+++ dev/Project13/html/js/app.view.js
@@ -17,6 +17,7 @@ app.view = async function () {
   function create(tagName) {
     new p5(p => {
       p.preload = function () {
+        // loadImage要放在preload，不然畫不出來
         ["../assets/word_background.png"].forEach(path => {
           imgs[path] = p.loadImage(path);
         })
